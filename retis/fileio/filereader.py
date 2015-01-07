a794c35963909a5e608159aa10611d3d2951fb75
Anders Lervik <andersle@gmail.com>
2015-01-07 12:02:47 +0100

    Added docstring

--- retis/fileio/filereader.py
+++ retis/fileio/filereader.py
@@ -5,6 +5,9 @@ filereader.py: This file handles input/output
 """
 import os
 def readinputfile(filename):
+    """
+    readinputfile: This function reads the input parameters
+    """
     if not os.path.isfile(filename):
         raise IOError('Could not locate file: {}'.format(filename))
 
