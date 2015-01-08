0d2b036c1fc8770c5ad6ad511caee7e00e0c1141
Anders Lervik <andersle@gmail.com>
2015-01-08 12:13:56 +0100

    Added module for force fields

---
+++ retis/forcefield/onedimpot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This file contains one-dimensional potentials
+"""
+
+class DoubleWell(object):
+    """
+    Double well potential
+    The parameters are: 
+        a = 
+        b = 
+        c =
+    """
+    def __init__(self, a=1, b=1, c=0.0):
+        self.dim = 1 # dimensionality, can be used for checking consistency
+        self.a, self.b, self.c = a, b, c
+
+    def potential(self, r):
+        """
+        one-dimensional double well potential
+        """
+        return self.a*r**4-self.b*(r-self.c)**2
+
+    def force(self, r):
+        """
+        force corresponding to one-dimensional double well potential
+        """
+        return -4.0*self.a*r**3+2.0*self.b*(r-self.c)
+
