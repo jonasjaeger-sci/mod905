9e1d3452bd5e2b0daca6414501e9020cb019a184
Anders Lervik <andersle@gmail.com>
2015-01-08 14:05:50 +0100

    Added module for natural constants

---
+++ retis/core/constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+constants.py
+This module defines some natural constants, in different units
+"""
+
+_kB = {'eV/K':8.6173324e-05, 'J/K': 1.3806488e-23}
+
+
+
