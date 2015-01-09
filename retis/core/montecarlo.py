4012259fa478f6b3bbdaaf2fb4d845308d69c290
Anders Lervik <andersle@gmail.com>
2015-01-09 09:28:39 +0100

    Updated random generator

--- retis/core/montecarlo.py
+++ retis/core/montecarlo.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module for Monte Carlo algorithms
+Module for Monte Carlo Algorithms
 """
 
-from random import randrange # can perhaps be replaced by numpy
 import numpy as np
+from numpy.random import RandomState
 
-def accept_reject(system, trial):
+rnd = RandomState() # this will be the random number generator
+
+def seed_random_generator(seed=1, rnd=rnd):
+    rnd.seed(seed)
+
+def accept_reject(system, trial, rnd=rnd):
     """
     Routine for accepting or rejecting a MC move
     """
     vtrial = system.evaluate_potential(trial) # potential energy of trial pos
     deltae = vtrial-system.epot
     pacc = np.exp(-system.beta*deltae)
-    throwdice = np.random.random()
+    throwdice = rnd.rand()
     status = False
     if throwdice < pacc:
         pos, vpot, status = trial, vtrial, True
     return pos, vpot, vtrial, status
 
-def max_displace_step(system, maxdx=0.01):
+def max_displace_step(system, maxdx=0.01, rnd=rnd):
     """
     MC routine. Displace positions randomly. Accept or reject.
     Returns new positions and their corresponding potential energy.
     If move is rejectec, return old positions and potential energy.
     """
-    idx = randrange(0,system.n) # select particle
+    idx = rnd.random_integers(0,system.n-1) # select particle
     trial = np.copy(system.r)
-    trial[idx] += 2.0*maxdx*(np.random.random(system.dim)-0.5) # displace
+    trial[idx] += 2.0*maxdx*(rnd.rand(system.dim)-0.5) # displace
     r, epot, etrial, status = accept_reject(system, trial)
     return status
+
