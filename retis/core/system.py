7b5e687425017cb9fe3ec313e65e88ffe57b3611
Anders Lervik <andersle@gmail.com>
2015-01-13 13:57:17 +0100

    Updated after suggestions from pylint

--- retis/core/system.py
+++ retis/core/system.py
@@ -11,11 +11,34 @@ __all__ = ['System']
 
 class System(object):
     """
-    This class defines a system
+    This class defines a generic system for simulation.
     """
     def __init__(self, N=0, dim=0, r=None, v=None, f=None, p=[],
                  forcefield=[], periodic=False, box=[], temperature=None,
                  units=None):
+        """ 
+        Initialization of the system.
+    
+        Parameters
+        ----------
+        self : 
+        N : int, optional. Number of particles.
+        dim : int optional. The dimensionality.
+        r : numpy.array, optional. The positions of the particles.
+        v : numpy.array, optional. The velocities of the particles.
+        f : numpy.array, optional. The forces on the particles.
+        p : list, optional. The id of the particles in a list.
+        forcefield : list, optional. The potential functions that consitute the 
+            force field. 
+        periodic : boolean, optional. True = the system has periodic boundaries. 
+        box : list, optional. System boundaries in the self.dim dimensions.
+        temperature : float, optional. The temperature of the system.
+
+        Returns
+        -------
+        N/A, but sets derived variables:
+        self.beta : float, inverse of (kB*T).
+        """
         self.N = N # number of particles
         self.dim = dim # dimensionality
         self.r = r # positions of particles
@@ -37,6 +60,27 @@ class System(object):
             self.beta = 1.0/(self.temperature*constants._kB[units])
 
     def add_particle(self, r=None, v=None, f=None, name=None):
+        """ 
+        Adds a particle to the system.
+    
+        Parameters
+        ----------
+        self : 
+        r : numpy.array, optional. The positions of the particle.
+        v : numpy.array, optional. The velocities of the particle.
+        f : numpy.array, optional. The forces on the particle.
+        name : string, optional. The id of the particle.
+
+        Returns
+        -------
+        N/A, but increments self.N and updates
+        self.r, self.v, self.f and self.p
+
+        Note
+        ----
+        If no arguments are given a particle with id='?' will be
+        created.
+        """
         if not r: r = np.zeros(self.dim)
         if not v: v = np.zeros(self.dim)
         if not f: f = np.zeros(self.dim)
@@ -52,34 +96,68 @@ class System(object):
             self.f = np.vstack([self.f, f])
         self.N += 1
 
-    def force(self):
-        self.f = self.evaluate_force()
-
-    def potential(self):
-        self.v_pot = self.evaluate_potential()
-
     def evaluate_force(self, r=None):
-        """
-        This function evaluates the force on the particles
-        in the system.
-        If r is given, these positions are used rather than
-        self.r to evaluate the force. This might be beneficial
-        for trial moves etc. in MC routines
-        Note: May be beneficial to call forcefield(system) later
+        """ 
+        Evaluate the forces
+    
+        Parameters
+        ----------
+        self : 
+        r : numpy.array, optional. The positions of the particle.
+            If r is not given, self.r will be used.
+
+        Returns
+        -------
+        The forces as a numpy.array
         """
         if r is None:
             return self.forcefield.evaluate_force(self.r)
         else:
             return self.forcefield.evaluate_force(r)
 
+    def force(self):
+        """ 
+        Updates self.f by calling self.evaluate_force()
+    
+        Parameters
+        ----------
+        self : 
+        
+        Returns
+        -------
+        N/A, but updates self.f
+        """
+        self.f = self.evaluate_force()
+
     def evaluate_potential(self, r=None):
         """
-        This function evaluate the potential energy of the system
-        If r is given, these positions are used rather than
-        self.r to evaluate the potential. This might be beneficial
-        for trial moves etc. in MC routines
+        Evaluate the potential energy
+    
+        Parameters
+        ----------
+        self : 
+        r : numpy.array, optional. The positions of the particle.
+            If r is not given, self.r will be used.
+        
+        Returns
+        -------
+        The scalar potential energy correspoding to the given r.
         """   
         if r is None:
             return self.forcefield.evaluate_potential(self.r)
         else:
             return self.forcefield.evaluate_potential(r)
+
+    def potential(self):
+        """ 
+        Updates self.v_pot by calling self.evaluate_potential()
+    
+        Parameters
+        ----------
+        self : 
+        
+        Returns
+        -------
+        N/A, but updates self.f
+        """
+        self.v_pot = self.evaluate_potential()
