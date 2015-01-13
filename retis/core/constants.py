c1d444ee3a0e830d6fc37de90f39a7ec24b96345
Anders Lervik <andersle@gmail.com>
2015-01-13 14:08:59 +0100

    Change constant, no longer protected

--- retis/core/constants.py
+++ retis/core/constants.py
@@ -6,7 +6,7 @@ constants.py
 This module defines some natural constants, in different units
 """
 
-_kB = {'eV/K':8.6173324e-05, 'J/K': 1.3806488e-23}
+kB = {'eV/K':8.6173324e-05, 'J/K': 1.3806488e-23}
 
 
 
