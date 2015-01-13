1b34157438ea01e989ee3a0808475671d6f6d756
Anders Lervik <andersle@gmail.com>
2015-01-13 16:38:52 +0100

    Updated with changes from pylint

--- retis/forcefield/potential.py
+++ retis/forcefield/potential.py
@@ -8,12 +8,60 @@ This class is subclassed in all potential functions
 class PotentialFunction(object):
     """Generic potential function"""
     def __init__(self, dim=1, desc=""):
+        """ 
+        Initiates the potential
+    
+        Parameters
+        ----------
+        self : 
+        dim : int, optional. Represents the dimensionality.
+        desc : string, optional. Description of the force field.
+
+        Returns
+        -------
+        N/A 
+        """
         self.dim = dim # dimensionality
         self.desc = desc
     def force(self, *args):
+        """ 
+        Function to evaluate the force.
+    
+        Parameters
+        ----------
+        self : 
+        *args : Not defined here, it's defined in the derived potentials.
+
+        Returns
+        -------
+        N/A 
+        """
         pass
     def potential(self, *args):
+        """ 
+        Function to evaluate the force.
+    
+        Parameters
+        ----------
+        self : 
+        *args : Not defined here, it's defined in the derived potentials.
+
+        Returns
+        -------
+        N/A 
+        """
         pass
     def __str__(self):
+        """
+        Return the string description of the potential. 
+    
+        Parameters
+        ----------
+        self : 
+
+        Returns
+        -------
+        self.desc
+        """
         return self.desc
 
