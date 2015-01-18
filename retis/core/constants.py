0a3a3a927f069333d3b1410346a33a42687ea5fb
Anders Lervik <andersle@gmail.com>
2015-01-18 13:17:06 +0100

    Added support for lj unit

--- retis/core/constants.py
+++ retis/core/constants.py
@@ -5,7 +5,7 @@ constants.py
 This module defines some natural constants, in different units
 """
 
-kB = {'eV/K':8.6173324e-05, 'J/K': 1.3806488e-23}
+kB = {'eV/K':8.6173324e-05, 'J/K': 1.3806488e-23, 'lj': 1.0}
 
 
 
