e88a38bf03267fcc8b5f4671f6a1a963fa675591
Anders Lervik <andersle@gmail.com>
2015-02-27 18:08:27 +0100

    Added example with doublewell wca

---
+++ examples/molecular_dynamics/md_wca/md_movie_2d.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+"""
+Example of running a MD NVE simulation
+"""
+from retis.core import Simulation, System, Box
+from retis.core.particlefunctions import * 
+from retis.core.integrators import VelocityVerlet
+from retis.core.units import CONVERT
+from retis.forcefield import ForceField, PairWCAnp, DoubleWellWCA
+from retis.io import WriteGromacs
+from retis.tools import lattice_simple_cubic
+import numpy as np 
+
+# set up potential function(s) and force field:
+wca = PairWCAnp(dim=2)
+wca_parameters = {'A': {'sigma': 1.0, 'epsilon': 1.0},
+                  'B': {'sigma': 1.0, 'epsilon': 1.0}}
+
+dwca = DoubleWellWCA(dim=2)
+dwca_parameters = {'types': [('B', 'B')], 'rzero': 1.0*(2.0**(1.0/6.0)),
+                   'height': 6.0, 'width': 0.25}
+#dwca_parameters = {'types': [('B', 'B')], 'rzero': 1.0*(2.0**(1.0/6.0)),
+#                   'height': 15.0, 'width': 0.5}
+
+
+forcefield = ForceField(potential=[wca, dwca], params=[wca_parameters, 
+                                                       dwca_parameters])
+
+colors = {'A': 'blue', 'B': 'magenta'}
+target_temperature = 2.0
+size = np.array([[0.0, 3.4], [0.0, 3.4]]) # in lj-units
+box = Box(size)
+ljsystem = System(temperature=target_temperature, units='lj', box=box)
+# generate some lattice points, this will give 9 points
+lattice = lattice_simple_cubic(box.size, spacing=1.0)
+for i,pos in enumerate(lattice):
+    ljsystem.add_particle(name='A', pos=pos, mass=1.0, ptype='A')
+# mutate two particles to be of type B
+ljsystem.particles.ptype[7] = 'B'
+ljsystem.particles.ptype[8] = 'B'
+npart = ljsystem.particles.npart
+print('Added {} particles to a simple square lattice'.format(npart))
+npart = float(npart)
+# generate velocities:
+ljsystem.particles.vel = np.random.normal(loc=0.0,
+                                        scale=np.sqrt(ljsystem.temperature['set']),
+                                        size=(npart,2))
+reset_momentum(ljsystem)
+ljsystem.forcefield = forcefield
+# also initiate forces:
+ljsystem.potential_and_force()
+
+# place N particles 
+write_gro = WriteGromacs('test.gro', box, frame=0, units=ljsystem.units)
+
+numberofsteps = 950
+simulation_eq = Simulation(endcycle=numberofsteps)
+timestep = 0.0025
+integrator = VelocityVerlet(timestep)
+
+task_integrate = {'func': integrator.integration_step,
+                  'args': [ljsystem]}
+simulation_eq.add_task(task_integrate)
+outfmt = '{0:8d} {1:12.7f} {2:12.7f} {3:12.7f} {4:12.7f}'
+step, v_pot, e_kin, e_tot, temperature = [], [], [], [], []
+time = []
+# we will animate on the fly:
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from matplotlib import rc
+import matplotlib.gridspec as gridspec
+
+gs = gridspec.GridSpec(2, 2)
+
+rc('axes', labelsize='large')
+rc('font', family='serif')
+
+fig = plt.figure(figsize=(12, 6))
+
+dx = size[0][1]-size[0][0]
+f = 0.12
+dx = np.array([-dx*f, dx*f])
+dy = size[1][1]-size[1][0]
+dy = np.array([-dy*f*0.1, dy*f*0.1])
+SIGMA = CONVERT['length']['lj', 'Å']
+ECONV = CONVERT['energy']['lj', 'kcal/mol']
+
+ax = fig.add_subplot(gs[:,0])
+
+ax.set_xlim((size[0]+dx)*SIGMA)
+ax.set_ylim((size[1]+dy)*SIGMA)
+ax.set_aspect('equal', 'datalim')
+ax.set_xlabel('$x/\AA{}$')
+ax.set_ylabel('$y/\AA{}$')
+ax.set_xticks([size[0][0]*SIGMA, size[0][1]*SIGMA])
+ax.set_yticks([size[1][0]*SIGMA, size[1][1]*SIGMA])
+ax.xaxis.labelpad=-5
+ax.yaxis.labelpad=-5
+pos = box.pbc_wrap(ljsystem.particles.pos)
+circles = [plt.Circle((p[0],p[1]), radius=SIGMA*0.5, alpha=.5) for p in pos]
+force_arrow = plt.quiver(pos[:,0], pos[:,1], zorder=4)
+plt.quiverkey(force_arrow,1,-4,6,'Forces',coordinates='data',color='black')
+vel_arrow = plt.quiver(pos[:,0], pos[:,1], color='darkgreen',zorder=4)
+plt.quiverkey(vel_arrow,4,-4,6,'Velocities',coordinates='data',color='darkgreen')
+linebond, = ax.plot(None, None, lw=2, ls='-', color='magenta', alpha=1)
+
+ax.axhline(y=size[1][0]*SIGMA, lw=2, ls=':', color='k')
+ax.axhline(y=size[1][1]*SIGMA, lw=2, ls=':', color='k')
+ax.axvline(x=size[0][0]*SIGMA, lw=2, ls=':', color='k')
+ax.axvline(x=size[0][1]*SIGMA, lw=2, ls=':', color='k')
+
+ax2 = fig.add_subplot(gs[0,1])
+ax2.set_xlim(0,5000)
+ax2.set_ylim(-0.6, 0.6)
+ax2.set_xlabel('Time/fs')
+ax2.set_ylabel('Energy/kcal/mol')
+time_text = ax2.text(0.02, 0.95, '', transform=ax2.transAxes)
+linepot, = ax2.plot(None, None, lw=2, ls='-', color='blue', label='Potential')
+linekin, = ax2.plot(None, None, lw=2, ls='-', color='green', label='Kinetic')
+linetot, = ax2.plot(None, None, lw=2, ls='-', color='k', label='Total')
+ax2.legend(loc='lower left', ncol=2, frameon=False)
+ax3 = fig.add_subplot(gs[1,1])
+ax3.set_xlim(0,10.0)
+
+rpot = np.linspace(0.1, 5, 1000)
+vpot = []
+pos1 = np.array([xi for xi in ljsystem.particles.pos[7]])
+pos2 = np.array([xi for xi in ljsystem.particles.pos[8]])
+size = np.array([[0.0, 10.0], [0.0, 10.0]]) # in lj-units
+fakebox = Box(size)
+for ri in rpot:
+    ljsystem.particles.pos[7] = np.array([0.0, 0.0])
+    ljsystem.particles.pos[8] = np.array([ri, 0.0])
+    v = dwca.potential(ljsystem.particles, fakebox)
+    vpot.append(v)
+ljsystem.particles.pos[7] = np.array([xi for xi in pos1])
+ljsystem.particles.pos[8] = np.array([xi for xi in pos2])
+print(ljsystem.particles.pos)
+linedwpot, = ax3.plot(rpot, vpot, lw=2, ls='-', color='black')
+ax3.set_ylim(0,dwca.height+1)
+min_max = dwca.min_max()
+ax3.set_xlim(min_max[0]-0.2, min_max[1]+0.2)
+orderscatter = ax3.scatter(None, None, alpha=0.5, color='green', marker='s', s=150)
+#plt.subplots_adjust(left=0.1, right=0.95, top=0.95, bottom=0.15)
+
+def init():
+    # function to draw a clear frame
+    patches = []
+    for ci in circles:
+        ci.set_visible(False)
+        ax.add_patch(ci)
+        patches.append(ci)
+    #linebond.set_data(None, None)
+    #patches.append(linebond)
+    #linepot.set_data(None, None)
+    #patches.append(linepot)
+    #linekin.set_data(None, None)
+    #patches.append(linekin)
+    #linetot.set_data(None, None)
+    #patches.append(linetot)
+    force_arrow.set_visible(False)
+    patches.append(force_arrow)
+    vel_arrow.set_visible(False)
+    patches.append(vel_arrow)
+    time_text.set_text('')
+    patches.append(time_text)
+    return patches #line1, line2, line3, line4
+
+def update(i):
+    pos = box.pbc_wrap(ljsystem.particles.pos)
+    #pos = ljsystem.particles.pos
+    vel = ljsystem.particles.vel
+    force = ljsystem.particles.force
+    ptypes = ljsystem.particles.ptype
+    patches = []
+    for ci, pi, itype in zip(circles, pos, ptypes):
+        ci.center = np.array([pi[0],pi[1]])*SIGMA
+        ci.set_color(colors[itype])
+        ci.set_visible(True)
+        patches.append(ci)
+    fmax = None
+    for fi in force:
+        fs = np.sqrt(np.dot(fi,fi))
+        if fmax is None or fs>fmax: fmax=fs
+    vmax = None
+    for vi in vel:
+        vs = np.sqrt(np.dot(vi,vi))
+        if vmax is None or vs>vmax: vmax = vs
+    XY, FU, FV, VU, VV = [], [], [], [], []
+    for pi, fi, vi in zip(pos, force, vel):
+        pj = pi*SIGMA
+        fj = 10.0*fi/fmax
+        vj = 10.0*vi/vmax
+        XY.append([pj[0], pj[1]])
+        FU.append(fj[0])
+        FV.append(fj[1])
+        VU.append(vj[0])
+        VV.append(vj[1])
+    force_arrow.set_offsets(XY)
+    force_arrow.set_UVC(FU, FV)
+    force_arrow.set_visible(True)
+    patches.append(force_arrow)
+    vel_arrow.set_offsets(XY)
+    vel_arrow.set_UVC(VU, VV)
+    vel_arrow.set_visible(True)
+    patches.append(vel_arrow)
+    if simulation_eq.is_finished():
+        print('Simulation is done')
+        return patches
+    else:
+        # reaction coordinate:
+        delta = box.pbc_dist_coordinate(ljsystem.particles.pos[7] -
+                                        ljsystem.particles.pos[8])
+        dr = np.sqrt(np.dot(delta, delta))
+        points = [dr, dwca.potential(ljsystem.particles, box)]
+        orderscatter.set_offsets(points)
+        patches.append(orderscatter)
+        write_gro.write_frame(pos, atomname=ljsystem.particles.ptype)
+        pos2 = pos[8,:] + delta
+        linebond.set_data([pos[8,0]*SIGMA, pos2[0]*SIGMA],[pos[8,1]*SIGMA, pos2[1]*SIGMA])
+        patches.append(linebond)
+        step.append(simulation_eq.cycle['step'])
+        time.append(simulation_eq.cycle['step']*timestep*CONVERT['time']['lj', 'fs'])
+        kin_tens = calculate_kinetic_energy_tensor(ljsystem)
+        temp, avgtemp, _ = calculate_kinetic_temperature(ljsystem, kin_tensor=kin_tens)
+        v_pot.append(ljsystem.v_pot/npart)
+        temperature.append(avgtemp)
+        e_kin.append(kin_tens.trace()/npart)
+        e_tot.append(e_kin[-1]+v_pot[-1])
+        linepot.set_data(time, (np.array(v_pot)-v_pot[0])*ECONV)
+        patches.append(linepot)
+        linekin.set_data(time, (np.array(e_kin)-e_kin[0])*ECONV)
+        patches.append(linekin)
+        linetot.set_data(time, (np.array(e_tot)-e_tot[0])*ECONV)
+        patches.append(linetot)
+        time_text.set_text('Time: {0:6.2f} fs'.format(time[-1]))
+        patches.append(time_text)
+        print(outfmt.format(step[-1], temperature[-1], v_pot[-1], e_kin[-1], e_tot[-1]))
+        simulation_eq.step()
+        return patches
+
+anim = animation.FuncAnimation(fig, update, frames=numberofsteps, repeat=False,
+                               interval=2, blit=True, init_func=init)
+#anim.save('particles.mp4', fps=30, extra_args=['-vcodec', 'libx264']) # for making movie file
+plt.show()
