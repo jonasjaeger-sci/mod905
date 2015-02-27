976f9517a57e65b76e5a1c28c737ba804bc0d300
Anders Lervik <andersle@gmail.com>
2015-02-27 15:54:43 +0100

    Update of NVE example, simplified

--- examples/molecular_dynamics/md_argon.py
+++ examples/molecular_dynamics/md_argon.py
@@ -1,137 +1,133 @@
 # -*- coding: utf-8 -*-
+"""
+Example of running a MD NVE simulation.
+This system considered is a simple Lennard-Jones fluid.
+"""
+# pylint: disable=C0103
 from __future__ import print_function
-"""
-Example of running a MD NVE simulation
-"""
 from retis.core import Simulation, System, Box
-from retis.core.particlefunctions import * 
+from retis.core.particlefunctions import (calculate_kinetic_energy_tensor,
+                                          calculate_kinetic_temperature,
+                                          calculate_scalar_pressure,
+                                          calculate_linear_momentum,
+                                          reset_momentum)
 from retis.core.integrators import VelocityVerlet
 from retis.forcefield import ForceField, PairLennardJonesCutnp
 from retis.io import WriteGromacs
 from retis.tools import latticefcc
-import numpy as np 
+import numpy as np
 
 # set up potential function(s) and force field:
-lennard_jones = PairLennardJonesCutnp()
-lj_parameters = {'Ar': {'sigma': 1.0, 'epsilon': 1.0, 'rcut': 2.5},
-              'mixing': 'geometric'}
-forcefield = ForceField(potential=[lennard_jones], params=[lj_parameters])
-
-target_temperature = 1.0
-target_density = 0.9
-lattice, size = latticefcc(density=target_density, nx=3, ny=3, nz=3)
+potential = PairLennardJonesCutnp(shift=True)
+forcefield = ForceField(potential=[potential],
+                        params=[{'Ar': {'sigma': 1.0,
+                                        'epsilon': 1.0,
+                                        'rcut': 2.5}}])
+lattice, size = latticefcc(density=0.9, nx=3, ny=3, nz=3)
 box = Box(size)
 
-ljsystem = System(temperature=target_temperature, units='lj', box=box)
+ljsystem = System(temperature=2.0, units='lj', box=box)
 for pos in lattice:
     ljsystem.add_particle(name='Ar', pos=pos, mass=1.0, ptype='Ar')
 npart = ljsystem.particles.npart
 print('Created fcc grid with {} atoms.'.format(npart))
 
 # generate velocities:
-ljsystem.particles.vel = np.random.normal(loc=0.0, 
-                                        scale=np.sqrt(ljsystem.temperature['set']),
-                                        size=(npart,3))
+scalet = np.sqrt(ljsystem.temperature['set'])
+ljsystem.particles.vel = np.random.normal(loc=0.0, scale=scalet,
+                                          size=(npart, 3))
+# we need to reset the momemtum now, since the initial velocities may have
+# a net drift
 reset_momentum(ljsystem)
-calculate_linear_momentum(ljsystem)
-ljsystem.adjust_dof([1,1,1])
 temp, avgtemp, _ = calculate_kinetic_temperature(ljsystem)
 print('Generated temperatures with average: {}'.format(avgtemp))
+# Attach force field:
 ljsystem.forcefield = forcefield
-# also initiate forces:
 ljsystem.potential_and_force()
-
+# initial trajectory writer
 write_gro = WriteGromacs('test.gro', box, frame=0, units=ljsystem.units)
-# finished with initialization
 
-
-# first set up a simulation where we equilibriate the system
-# and scale the temperature:
-numberofsteps = 100
-simulation_eq = Simulation(endcycle=numberofsteps)
-integrator = VelocityVerlet(0.0025)
+# set up a simple NVE simulation
+numberofsteps = 2000
+simulationNVE = Simulation(endcycle=numberofsteps)
+integrator = VelocityVerlet(0.002)
 
 task_integrate = {'func': integrator.integration_step,
                   'args': [ljsystem]}
 
-def rescale_velocity(system):
-    temp, avgtemp, _ = calculate_kinetic_temperature(ljsystem)
-    scale_factor = np.sqrt(system.temperature['set']/avgtemp)
-    system.particles.vel *= scale_factor
+simulationNVE.add_task(task_integrate)
 
-task_rescale = {'func': rescale_velocity, 'args': [ljsystem]}
-
-def reset_momentum_task(system):
-    print('Resetting momentum')
-    reset_momentum(system)
-
-task_reset_momentum = {'func': reset_momentum_task, 'args': [ljsystem],
-                       'extra': {'every': 10}}
-
-simulation_eq.add_task(task_integrate)
-simulation_eq.add_task(task_rescale)
-simulation_eq.add_task(task_reset_momentum)
 
 def common_calculations(system):
+    """
+    This function defines some common calculations for the system.
+    It used functions from the particle functions module to obtain
+    energies, pressure, etc.
+    """
     kin_tens = calculate_kinetic_energy_tensor(system)
-    press_tens = calculate_pressure_tensor(system, 
-                                           kin_tensor=kin_tens)
-    temp, avgtemp, _ = calculate_kinetic_temperature(system, 
-                                                     kin_tensor=kin_tens)
-    ekin = kin_tens.trace()
-    press =  calculate_scalar_pressure(system, press_tensor=press_tens, 
-                                       kin_tensor=kin_tens)
-    pxx = press_tens.diagonal()
-    vpot = system.v_pot
-    etot = ekin+vpot
-    return vpot, ekin, etot, avgtemp, press, pxx
+    _, tempi, _ = calculate_kinetic_temperature(system,
+                                                kin_tensor=kin_tens)
+    ekini = kin_tens.trace()
+    pressi = calculate_scalar_pressure(system, kin_tensor=kin_tens)
+    vpoti = system.v_pot
+    etoti = ekini + vpoti
+    momi = calculate_linear_momentum(system)
+    return vpoti, ekini, etoti, tempi, pressi, momi
 
 temps = []
 kinetic_e = []
 potential_e = []
 total_e = []
 pressure = []
-
-while not simulation_eq.is_finished():
-    # let us also calculate some stuff:
-    vpot, ekin, etot, avgtemp, press, pxx = common_calculations(ljsystem)
+step = []
+momentum = []
+outfmt = '{0:8d} {1:12.7f} {2:12.7f} {3:12.7f} {4:12.7f} {5:12.7f}'
+outfmt2 = '# {0:>6s} {1:>12s} {2:>12s} {3:>12s} {4:>12s} {5:>12s}'
+print(outfmt2.format('Step', 'Temp', 'Pot', 'Kin', 'Total', 'Press'))
+while not simulationNVE.is_finished():
+    step.append(simulationNVE.cycle['step'])
+    vpot, ekin, etot, avgtemp, press, mom = common_calculations(ljsystem)
     temps.append(avgtemp)
-    kinetic_e.append(ekin)
-    potential_e.append(vpot)
-    total_e.append(etot)
-    pressure.append(pxx)
-    print('Step: {}, Temp: {}, E-tot:{}'.format(simulation_eq.cycle['step'], avgtemp, etot))
+    kinetic_e.append(ekin / npart)
+    potential_e.append(vpot / npart)
+    total_e.append(etot / npart)
+    pressure.append(press)
+    momentum.append(mom)
+    print(outfmt.format(step[-1], temps[-1], potential_e[-1], kinetic_e[-1],
+                        total_e[-1], pressure[-1]))
     write_gro.write_frame(ljsystem.particles.pos)
-    simulation_eq.step()
+    simulationNVE.step()
 
-simulation_nve = Simulation(endcycle=numberofsteps)
-task_reset_momentum = {'func': reset_momentum_task, 'args': [ljsystem],
-                       'extra': {'every': 100}}
-simulation_nve.add_tasks(task_integrate, task_reset_momentum)
-
-while not simulation_nve.is_finished():
-    vpot, ekin, etot, avgtemp, press, pxx = common_calculations(ljsystem)
-    temps.append(avgtemp)
-    kinetic_e.append(ekin)
-    potential_e.append(vpot)
-    total_e.append(etot)
-    pressure.append(pxx)
-    print('Step: {}, Temp: {}, E-tot:{}'.format(simulation_nve.cycle['step'], avgtemp, etot))
-    write_gro.write_frame(ljsystem.particles.pos)
-    simulation_nve.step()
-
-
+# make some plots of results from the simulation:
 from matplotlib import pyplot as plt
-plt.figure()
-plt.plot(temps)
-plt.show()
-plt.figure()
-plt.plot(kinetic_e-kinetic_e[numberofsteps])
-plt.plot(potential_e-potential_e[numberofsteps])
-plt.plot(total_e-total_e[numberofsteps])
-plt.figure()
-pressure = np.array(pressure)
-for pxx in pressure.T:
-    plt.plot(pxx)
-plt.show()
+import matplotlib.gridspec as gridspec
 
+gs = gridspec.GridSpec(2, 2)
+fig = plt.figure(figsize=(12, 6))
+ax1 = fig.add_subplot(gs[:, 0])
+ax1.set_ylabel('Energy per particle')
+ax1.plot(step, potential_e, lw=4, ls='-',
+         color='b', alpha=0.5, label='Potential')
+ax1.plot(step, kinetic_e, lw=4, ls='-',
+         color='g', alpha=0.5, label='Kinetic')
+ax1.plot(step, total_e, lw=4, ls='-',
+         color='k', alpha=0.5, label='Total')
+ax1.legend()
+ax1.set_xlabel('Step')
+ax2 = fig.add_subplot(gs[0, 1])
+ax2.set_ylabel('Pressure')
+ax2.plot(step, pressure, lw=4, ls='-',
+         color='b', alpha=0.5, label='Pressure')
+ax2.legend()
+momentum = np.array(momentum)
+ax3 = fig.add_subplot(gs[1, 1])
+ax3.plot(step, momentum[:, 0], lw=4, ls='-',
+         color='b', alpha=0.5, label='x-direction')
+ax3.plot(step, momentum[:, 1], lw=4, ls='-',
+         color='g', alpha=0.5, label='y-direction')
+ax3.plot(step, momentum[:, 2], lw=4, ls='-',
+         color='k', alpha=0.5, label='z-direction')
+ax3.set_xlabel('Step')
+ax3.set_ylabel('Linear momentum')
+fig.subplots_adjust(bottom=0.1, right=0.95, top=0.9, left=0.12, wspace=0.3)
+plt.show()
