0eab3122709af8f36c07dd59c925c357a1d145d3
Anders Lervik <andersle@gmail.com>
2015-06-16 16:07:50 +0200

    Added method for full analysis

--- retis/analysis/path_analysis.py
+++ retis/analysis/path_analysis.py
@@ -6,10 +6,11 @@ in the object PathEnsemble in retis.core.path
 from __future__ import absolute_import
 import numpy as np
 from .analysis import running_average, block_error
-
+from .histogram import histogram
 
 __all__ = ['running_pcross', 'pcross_lambda', 'pcross_error',
-           'get_path_distribution', 'shoot_analysis']
+           'get_path_distribution', 'shoot_analysis',
+           'analyse_path_ensemble']
 
 
 def _get_successfull(pathensemble, idetect):
@@ -204,7 +205,7 @@ def pcross_error(pathensemble, idetect, maxblock=5000, blockskip=1,
     return blen, berr, berr_avg, rel_err, avg_rel_err, ncor, avg_ncor
 
 
-def get_path_distribution(pathensemble):
+def get_path_distribution(pathensemble, bins=1000):
     """
     This function will get the distribution of path-lengths.
 
@@ -212,13 +213,17 @@ def get_path_distribution(pathensemble):
     ----------
     pathensemble : object of type retis.core.path.PathEnsemble
         This is the PathEnsemble we will analyse
+    bins : int, optional
+        The number of bins to use for the histograms for the distribution.
 
     Returns
     -------
-    out[0] : numpy.array
-        The length of the accepted paths
-    out[1] : numpy.array
-        The length of all paths
+    out[0] : list, [numpy.array, numpy.array]
+        Result for accepted paths (distribution). out[0][0] is the histogram
+        and out[0][1] are the mid points for bins.
+    out[1] : list, [numpy.array, numpy.array]
+        Result for all paths (distribution). out[1][0] is the histogram and
+        out[1][1] are the mid points for bins.
     """
     # first get lengths of accepted paths:
     length_acc = [path['length'] for path in pathensemble.get_accepted()]
@@ -233,25 +238,37 @@ def get_path_distribution(pathensemble):
         else:
             raise ValueError('Unknown mc move: {}'.format(move))
     length_all = np.array(length_all)
-    return length_acc, length_all
+    hist1, _, bin_mid1 = histogram(length_acc, bins=bins,
+                                   limits=(length_acc.min(), length_acc.max()),
+                                   density=True)
+    hist2, _, bin_mid2 = histogram(length_all, bins=bins,
+                                   limits=(length_all.min(), length_all.max()),
+                                   density=True)
+    return [hist1, bin_mid1], [hist2, bin_mid2]
 
 
-def shoot_analysis(pathensemble):
+def shoot_analysis(pathensemble, bins=1000):
     """
     This method will do a shoot analysis of the pathensemble.
 
     Parameters
     ----------
     pathensemble : object of type retis.core.path.PathEnsemble
-        This is the PathEnsemble we will analyse
+        This is the PathEnsemble we will analyse.
+    bins : int, optional
+        The number of bins to use for the histograms for the distribution.
 
     Returns
     -------
-    out : dict
+    out[0] : dict
         For each possible status ('ACC, 'BWI', etc) this dict will contain
-        a numpy.array of the order parameter in the shooting point.
+        a histogram as returned by the histogram function.
         It will also contain a 'REJ' key which is the concatenation of all
         rejected and a 'ALL' key which is simply all the values.
+    out[1] : dict
+        For each possible status ('ACC, 'BWI', etc) this dict will contain
+        the scale factors for the histograms. The scale factors are obtained
+        by dividing with the 'ALL' value.
     """
     shoot_stats = {'REJ':[], 'ALL':[]}
     for path in pathensemble.paths:
@@ -265,6 +282,57 @@ def shoot_analysis(pathensemble):
             if status != 'ACC':
                 shoot_stats['REJ'].append(orderp)
             shoot_stats['ALL'].append(orderp)
+    histograms = {}
+    scale = {}
     for key in shoot_stats:
         shoot_stats[key] = np.array(shoot_stats[key])
-    return shoot_stats
+        mind = shoot_stats[key].min()
+        maxd = shoot_stats[key].max()
+        histograms[key] = histogram(shoot_stats[key], bins=bins,
+                                    limits=(mind, maxd), density=True)
+        scale[key] = (float(len(shoot_stats[key])) /
+                      float(len(shoot_stats['ALL'])))
+    return histograms, scale
+
+def analyse_path_ensemble(path_ensemble, analysis_settings, output=None,
+                          idetect=None):
+    """
+    This method will make use of the different analysis functions and analyse
+    a path ensemble. It will also output the results using the specified
+    output object.
+
+    Parameters
+    ----------
+    path_ensemble : object of type PathEnsemble
+        The Path ensemble to analyse
+    analysis_settings : dict
+        This contains settings for the analysis
+    output : object, optional
+        This object handles the output, if not given this method
+        will just return the analysis results.
+    idetect : float, optional
+        This is the interface used for detecting if a path is usccessfull
+        or not. If no value is given, path_ensemble.interfaces[-1] will be
+        use
+    """
+    if idetect is None:
+        idetect = path_ensemble[-1]
+    # first analysis is pcross as a function of lambda:
+    pcross, lamb = pcross_lambda(path_ensemble,
+                                 ngrid=analysis_settings['ngrid'])
+    # next get the running average of the crossing probability
+    prun, pdata = running_pcross(path_ensemble, idetect)
+    # next, the error analysis:
+    error = pcross_error(path_ensemble, idetect, data=pdata,
+                         maxblock=analysis_settings['maxblock'],
+                         blockskip=analysis_settings['blockskip'])
+    # next length-analysis:
+    hist1, hist2 = get_path_distribution(path_ensemble,
+                                         bins=analysis_settings['bins'])
+    # next, shoots:
+    # move so that the analysis returns histograms and scale...
+    hist3, scale = shoot_analysis(path_ensemble, bins=analysis_settings['bins'])
+    if output is not None:
+        # create the output - this is handled by the object
+        output.output_pcross_lambda(lamb, pcross)
+    return pcross, lamb, prun, pdata, error, hist1, hist2, hist3, scale
