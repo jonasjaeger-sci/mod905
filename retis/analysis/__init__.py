38e2ab2a41537736162072a6c6494f817caa9286
Anders Lervik <andersle@gmail.com>
2015-06-22 15:35:35 +0200

    Update of docs

--- retis/analysis/__init__.py
+++ retis/analysis/__init__.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
-from .histogram import *
-from .analysis import *
+"""
+This package defines analysis tools for the pyretis program.
+
+Package structure
+=================
+
+Modules:
+
+- __init__.py: Imports from the other modules.
+
+- histogram.py: Defines methods useful for generating histograms.
+
+- analysis.py: Defines methods for numerical analysis.
+
+- path_analysis.py: Defines methods for analysis of path ensembles.
+"""
+from .histogram import histogram, match_all_histograms
+from .analysis import running_average, block_error
+from .path_analysis import analyse_path_ensemble, match_probabilities
