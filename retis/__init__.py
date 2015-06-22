c3f54d1fd5be2dd563119680d1f62f1f3a631649
Anders Lervik <andersle@gmail.com>
2015-06-22 14:17:18 +0200

    Added files for holding the version

--- retis/__init__.py
+++ retis/__init__.py
@@ -3,7 +3,11 @@
 This is the RETIS library
 =========================
 """
+from __future__ import absolute_import
 from . import core
 from . import forcefield
 from . import analysis
 from . import tools
+
+from .version import VERSION
+__version__ = VERSION
