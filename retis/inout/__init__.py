8693fcbc079920456b8b81b89e8f0f984762e8d8
Anders Lervik <andersle@gmail.com>
2015-07-06 18:21:54 +0200

    Added the get_predefined_table to __init__

--- retis/inout/__init__.py
+++ retis/inout/__init__.py
@@ -3,7 +3,8 @@
 This library handles the input and output to pytismol.
 ======================================================
 """
-from .trajectoryio import WriteXYZ, WriteGromacs
+from .txtinout import get_predefined_table
+from .trajectoryio import WriteXYZ, WriteGromacs, create_traj_writer
 from .report import generate_report
 from .analysisio import (mpl_output_analysis, mpl_total_probability,
                          txt_output_analysis, txt_total_probability)
