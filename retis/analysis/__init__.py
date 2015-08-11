ad4d587125aa1b3d24f732bfe841b539783d1f55
Anders Lervik <andersle@gmail.com>
2015-08-11 17:49:44 +0200

    Added reference to eergy_analysis.py

--- retis/analysis/__init__.py
+++ retis/analysis/__init__.py
@@ -9,12 +9,15 @@ Modules:
 
 - __init__.py: Imports from the other modules.
 
+- analysis.py: Defines methods for numerical analysis.
+
 - histogram.py: Defines methods useful for generating histograms.
 
-- analysis.py: Defines methods for numerical analysis.
+- energy_analysis.py: Defines methods useful for analysing the energy output.
 
 - path_analysis.py: Defines methods for analysis of path ensembles.
 """
-from .histogram import histogram, match_all_histograms
 from .analysis import running_average, block_error, block_error_corr
+from .histogram import histogram, match_all_histograms
+from .energy_analysis import analyse_energy
 from .path_analysis import analyse_path_ensemble, match_probabilities
