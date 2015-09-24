c3ad337412b16388408a739d02bf0efd215f3951
Anders Lervik <andersle@gmail.com>
2015-09-24 10:45:16 +0200

    Updated flux analysis.
    Now all fluxes are calculated and properly stored.

--- retis/analysis/flux_analysis.py
+++ retis/analysis/flux_analysis.py
@@ -4,13 +4,12 @@ This file contains methods for analysis of crossings for the
 flux data
 """
 from __future__ import absolute_import
-#from .analysis import analyse_data
 import numpy as np
 
 __all__ = ['analyse_flux']
 
 
-def analyse_flux(fluxdata, interfaces, endstep, settings):
+def analyse_flux(fluxdata, interfaces, settings, end_step, time_step):
     """
     This method will run the analysis on several order parameters and
     collect the results into a structure which is convenient for plotting.
@@ -22,10 +21,12 @@ def analyse_flux(fluxdata, interfaces, endstep, settings):
         for the fluxes.
     interfaces : list of floats
         These are the interfaces used in the simulation.
-    endstep : int
-        This is the last step done in the simulation.
     settings : dict
         This dict contains the settings for the analysis.
+    end_step : int
+        This is the last step done in the simulation.
+    time_step : int
+        This is the time step used in the simulation.
 
     Returns
     -------
@@ -33,20 +34,21 @@ def analyse_flux(fluxdata, interfaces, endstep, settings):
 
     """
     results = {}
-    eff_cross, ncross, neffcross, times = _effective_crossings(fluxdata,
-                                                               len(interfaces),
-                                                               endstep)
-    results['effective crossings'] = eff_cross
-    results['ncross'] = ncross
-    results['neffcross'] = neffcross
-    results['times'] = times
-    time_step = 0.002
-    _calculate_flux(eff_cross[0], times['OA'], settings['skipcross'],
-                    time_step)
+    ret = _effective_crossings(fluxdata, len(interfaces), end_step)
+    results = {'eff_cross': ret[0],
+               'ncross': ret[1],
+               'effcross': ret[2],
+               'times': ret[3]}
+    for i in range(len(interfaces)):
+        time, ncross, flux = _calculate_flux(results['eff_cross'][i],
+                                             results['times']['OA'],
+                                             settings['skipcross'], time_step)
+        results['flux-{}'.format(i + 1)] = np.column_stack((time, ncross,
+                                                            flux))
     return results
 
 
-def _effective_crossings(fluxdata, nint, endstep):
+def _effective_crossings(fluxdata, nint, end_step):
     """
     This method will analyse the flux output and obtain the effective
     crossings.
@@ -58,12 +60,13 @@ def _effective_crossings(fluxdata, nint, endstep):
         for the fluxes.
     nint : int
         The number of interfaces used.
-    endstep : int
+    end_step : int
         This is the last step done in the simulation.
 
     Returns
     -------
-    eff_cross : list
+    eff_cross : list of lists
+        eff_cross[i] is the effective crossings times for interface i.
     ncross : list of ints
         ncross[i] = the number of crossings for interface i.
     neffcross : list of ints
@@ -74,8 +77,8 @@ def _effective_crossings(fluxdata, nint, endstep):
 
     Note
     ----
-    We do here a lot of intf - 1. This is just to be compatible with the old
-    fortran code where the interfaces are numberes 1, 2, 3 rather than 0, 1, 2.
+    We do here intf - 1. This is just to be compatible with the old fortran
+    code where the interfaces are numberes 1, 2, 3 rather than 0, 1, 2.
     If this is to be changed in the future the '-1' can just be removed.
     """
     # first line is used to determine if we start in B or A
@@ -83,10 +86,10 @@ def _effective_crossings(fluxdata, nint, endstep):
     firstcross = [False] * nint
     ncross = [0] * nint
     neffcross = [0] * nint
+    eff_cross = {key: [] for key in range(nint)}
     end = {'A': 0, 'B': 0, 'OA': 0, 'OB': 0}
     start = {'A': 0, 'B': 0, 'OA': 0, 'OB': 0}
     time_in_state = {'A': 0, 'B': 0, 'OA': 0, 'OB': 0}
-    eff_cross = {key: [] for key in range(nint)}
     time, intf, sign = None, None, None
     for (time, intf, sign) in fluxdata:
         if sign > 0:  # positive direction
@@ -119,13 +122,13 @@ def _effective_crossings(fluxdata, nint, endstep):
                 time_in_state['B'] += (end['B'] - start['B'])
     # now, just add up the remaining:
     state = 'OA' if overallstate_a else 'OB'
-    time_in_state[state] += (endstep - start[state])
+    time_in_state[state] += (end_step - start[state])
     if intf - 1 == 0 and sign < 0:
         # note that the sign < 0 works for sign=None
-        time_in_state['A'] += (endstep - start['A'])
+        time_in_state['A'] += (end_step - start['A'])
     elif intf - 1 == 2 and sign > 0:
         # note that the sign > 0 works for sign=None
-        time_in_state['B'] += (endstep - start['B'])
+        time_in_state['B'] += (end_step - start['B'])
     return eff_cross, ncross, neffcross, time_in_state
 
 
@@ -157,7 +160,6 @@ def _calculate_flux(effective_cross, time_in_state, time_window, time_step):
     delta_t = time_step * time_window
     max_windows = int(1.0 * time_in_state / time_window)
     ncross = np.zeros(max_windows, dtype=np.int)
-    #ncross = {}
     for crossing in effective_cross:
         idx = np.floor((crossing[0] - 0.0) / time_window)
         ncross[idx] += 1
