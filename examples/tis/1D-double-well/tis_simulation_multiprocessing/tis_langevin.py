b038b0f659140e402cf0dfaafb112e750ffd7ba3
Anders Lervik <anders.lervik@ntnu.no>
2015-11-06 14:43:58 +0100

    Added two examples for serial/parallel tis

---
+++ examples/tis/1D-double-well/tis_simulation_multiprocessing/tis_langevin.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+This is a convenience script for creating TIS simulations.
+"""
+# pylint: disable=C0103
+from __future__ import print_function
+import multiprocessing
+import numpy as np
+import os
+from pyretis.core.path import create_path_ensembles
+from pyretis.core import Box, System
+from pyretis.core.simulation import create_simulation
+from pyretis.forcefield import ForceField
+from pyretis.forcefield.potentials import DoubleWell
+from pyretis.core.orderparameter import OrderParameterPosition
+from pyretis.inout import create_output
+
+
+simulation_settings = {'type': 'TIS',
+                       'integrator': {'name': 'Langevin', 'timestep': 0.002,
+                                      'gamma': 0.3, 'seed': 10,
+                                      'high-friction': False},
+                       'endcycle': 20000,
+                       'temperature': 0.07,
+                       'interfaces': [-0.9, -0.8, -0.7, -0.6,
+                                      -0.5, -0.4, -0.3, 1.0],
+                       'periodic_boundary': [False],
+                       'units': 'lj',
+                       'generate-vel': {'seed': 0, 'momentum': False,
+                                        'distribution': 'maxwell'},
+                       'tis': {'start_cond': 'L',
+                               'freq': 0.5,
+                               'maxlength': 10000,
+                               'aimless': True,
+                               'allowmaxlength': False,
+                               'sigma_v': -1,
+                               'seed': 10,
+                               'initial_path': 'kick'},
+                       'output': [{'type': 'pathensemble', 'target': 'file',
+                                   'when': {'every': 10}},
+                                  {'type': 'trialpath', 'target': 'file',
+                                   'when': {'every': 100}}]}
+
+
+def set_up_tis_simulation(settings):
+    """Method to do set-up for the TIS simulations"""
+    box = Box(periodic=settings['periodic_boundary'])
+    system = System(temperature=settings['temperature'],
+                    units=settings['units'],
+                    box=box)
+    system.add_particle(name='A', pos=np.array([-1.0]))
+    msg = 'Added one particle {} at position: {}'
+    if 'generate-vel' in settings:
+        system.generate_velocities(**settings['generate-vel'])
+        msg = 'Generated temperatures with average: {}'
+    # Set up force field:
+    double_well = DoubleWell(a=1.0, b=2.0, c=0.0)
+    forcefield = ForceField(potential=[double_well], desc='Double Well')
+    system.forcefield = forcefield
+    # add order parameter:
+    orderparameter = OrderParameterPosition('position', 0, dim='x', periodic=False)
+    settings['orderparameter'] = orderparameter
+    simulation_tis = create_simulation(settings, system)
+    return simulation_tis
+
+
+def run_simulation(simulation, settings):
+    ensemble = simulation.path_ensemble.ensemble
+    interfaces = simulation.path_ensemble.interfaces
+    output = [task for task in create_output(simulation.system, settings)]
+    print('Running:', ensemble)
+    for result in simulation.run():
+        step = result['cycle']
+        #result['traj'] = simulation.system
+        for task in output:
+            task.output(result)
+    print('Done with:', ensemble)
+    return True
+
+
+if __name__ == '__main__':
+    print('Simulation type: {}'.format(simulation_settings['type']))
+    print('Setting up TIS simulations:')
+    interfaces = simulation_settings['interfaces']
+    ensembles, detect = create_path_ensembles(interfaces, include_zero=False)
+
+    simulations = []
+
+    for i, path_ensemble in enumerate(ensembles):
+        ensemble = '{:03d}'.format(i+1)
+        print('\nPath ensemble: {} (no. {})'.format(path_ensemble.ensemble,
+                                                    ensemble))
+        # create a home for the simulation:
+        if not os.path.exists(ensemble):
+            print('* Creating folder "{}"'.format(ensemble))
+            os.makedirs(ensemble)
+        else:
+            print('* Folder "{}" already exist, will use it.'.format(ensemble))
+        path_file = os.path.join(ensemble, 'path.dat')
+        ensemble_file = os.path.join(ensemble, 'pathensemble.dat')
+        settings = {'type': 'TIS',
+                    'integrator': {'name': 'Langevin', 'timestep': 0.002,
+                                   'gamma': 0.3, 'seed': 10,
+                                   'high-friction': False},
+                    'endcycle': 20000,
+                    'temperature': 0.07,
+                    'periodic_boundary': [False],
+                    'units': 'lj',
+                    'generate-vel': {'seed': 0, 'momentum': False,
+                                     'distribution': 'maxwell'},
+                    'tis': {'start_cond': 'L',
+                            'freq': 0.5,
+                            'maxlength': 10000,
+                            'aimless': True,
+                            'allowmaxlength': False,
+                            'sigma_v': -1,
+                            'seed': 10,
+                            'initial_path': 'kick'}}
+        settings['path-ensemble'] = path_ensemble
+        settings['interfaces'] = path_ensemble.interfaces
+        settings['output'] = [{'type': 'pathensemble',
+                               'target': 'file',
+                               'filename': ensemble_file, 'use': True},
+                              {'type': 'trialpath',
+                               'target': 'file',
+                               'filename': path_file, 'use': False}]
+        simulation_tis = set_up_tis_simulation(settings)
+        proc = multiprocessing.Process(target=run_simulation,
+                                       args=(simulation_tis, settings))
+        simulations.append(proc)
+        proc.start()
+    #    simulations.append(simulation_tis)
+    #pool = multiprocessing.Pool(processes=4)              # start 4 worker processes
+    #result =  pool.map(run_simulation, simulations)
+    #for proc in simulations:
+    #    proc.join()
+    #    print('name: {}, exit-code: {}'.format(proc.name, proc.exitcode))
+    #print('Hello')
+    #for sim in simulations:
+    #    sim.join()
+    #    print('{} exitcode = {}'.format(sim.name, sim.exitcode))
+
+
+#    for sim in simulations:
+#        print(sim.output_task[0].writer)
+#        print(sim.output_task[0].writer.get_mode())
+#    pool = multiprocessing.Pool(processes=3)
+#results = pool.map_async(run_simulation, simulations)
+#results = pool.map(run_simulation, simulations)
+#    results = [pool.apply(run_simulation, args=(sim,)) for sim in simulations]
+#print(results)
---
+++ examples/tis/1D-double-well/tis_simulation_serial/tis_langevin.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+This is a convenience script for creating TIS simulations.
+"""
+# pylint: disable=C0103
+from __future__ import print_function
+import numpy as np
+import os
+from pyretis.core.path import create_path_ensembles
+from pyretis.core import Box, System
+from pyretis.core.simulation import create_simulation
+from pyretis.forcefield import ForceField
+from pyretis.forcefield.potentials import DoubleWell
+from pyretis.core.orderparameter import OrderParameterPosition
+
+
+simulation_settings = {'type': 'TIS',
+                       'integrator': {'name': 'Langevin', 'timestep': 0.002,
+                                      'gamma': 0.3, 'seed': 10,
+                                      'high-friction': False},
+                       'endcycle': 20000,
+                       'temperature': 0.07,
+                       'interfaces': [-0.9, -0.8, -0.7, -0.6,
+                                      -0.5, -0.4, -0.3, 1.0],
+                       'periodic_boundary': [False],
+                       'units': 'lj',
+                       'generate-vel': {'seed': 0, 'momentum': False,
+                                        'distribution': 'maxwell'},
+                       'tis': {'start_cond': 'L',
+                               'freq': 0.5,
+                               'maxlength': 10000,
+                               'aimless': True,
+                               'allowmaxlength': False,
+                               'sigma_v': -1,
+                               'seed': 10,
+                               'initial_path': 'kick'},
+                       'output': [{'type': 'pathensemble', 'target': 'file',
+                                   'when': {'every': 10}},
+                                  {'type': 'trialpath', 'target': 'file',
+                                   'when': {'every': 100}}]}
+
+
+def set_up_tis_simulation(settings):
+    """Method to do set-up for the TIS simulations"""
+    box = Box(periodic=settings['periodic_boundary'])
+    system = System(temperature=settings['temperature'],
+                    units=settings['units'],
+                    box=box)
+    system.add_particle(name='A', pos=np.array([-1.0]))
+    msg = 'Added one particle {} at position: {}'
+    if 'generate-vel' in settings:
+        system.generate_velocities(**settings['generate-vel'])
+        msg = 'Generated temperatures with average: {}'
+    # Set up force field:
+    double_well = DoubleWell(a=1.0, b=2.0, c=0.0)
+    forcefield = ForceField(potential=[double_well], desc='Double Well')
+    system.forcefield = forcefield
+    # add order parameter:
+    orderparameter = OrderParameterPosition('position', 0, dim='x', periodic=False)
+    settings['orderparameter'] = orderparameter
+    simulation_tis = create_simulation(settings, system)
+    return simulation_tis
+
+print('Simulation type: {}'.format(simulation_settings['type']))
+print('Setting up TIS simulations:')
+interfaces = simulation_settings['interfaces']
+stateA = interfaces[0]
+stateB = interfaces[-1]
+ensembles, detect = create_path_ensembles(interfaces, include_zero=False)
+
+for i, path_ensemble in enumerate(ensembles):
+    ensemble = '{:03d}'.format(i+1)
+    print('\nPath ensemble: {} (no. {})'.format(path_ensemble.ensemble,
+                                                ensemble))
+    # create a home for the simulation:
+    if not os.path.exists(ensemble):
+        print('* Creating folder "{}"'.format(ensemble))
+        os.makedirs(ensemble)
+    else:
+        print('* Folder "{}" already exist, will use it.'.format(ensemble))
+    path_file = os.path.join(ensemble, 'path.dat')
+    ensemble_file = os.path.join(ensemble, 'pathensemble.dat')
+    settings = {}
+    for key in simulation_settings:
+        try:
+            settings[key] = simulation_settings[key].copy()
+        except AttributeError:
+            settings[key] = simulation_settings[key]
+    settings['path-ensemble'] = path_ensemble
+    settings['interfaces'] = path_ensemble.interfaces
+    settings['output'] = [{'type': 'pathensemble',
+                           'target': 'file',
+                           'filename': ensemble_file,
+                           'when': {'every': 10}},
+                          {'type': 'trialpath',
+                           'target': 'file',
+                           'filename': path_file,
+                           'when': {'every': 100}}]
+    simulation_tis = set_up_tis_simulation(settings)
+    print('Created:', simulation_tis)
+    print('Starting', path_ensemble.ensemble)
+    for result in simulation_tis.run():
+        cycle = result['cycle']['step']
+    print('Done with', path_ensemble.ensemble)
+
