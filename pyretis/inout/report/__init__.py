b3e6390030220218057d0985779a239eb48e534d
Anders Lervik <anders.lervik@ntnu.no>
2015-11-10 16:52:19 +0100

    Forgot to add file

---
+++ pyretis/inout/report/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""This file contains methods for generating reports.
+
+The reports will typically summarize the results from different
+analysis and present it as a text file, pdf or web-page.
+
+Important functions
+-------------------
+
+"""
+from __future__ import absolute_import
+from pyretis.inout.report.report import generate_report_tis, generate_report_md
