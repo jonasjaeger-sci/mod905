78ba105ab971fcb07face0770be359d781c7aca5
Anders Lervik <anders.lervik@ntnu.no>
2015-11-12 09:00:51 +0100

    Made new function to which returns a file object.
    The new function will return a file object given a file type and
    a file name. It can be used to conveniently open files.

--- pyretis/inout/fileinout/__init__.py
+++ pyretis/inout/fileinout/__init__.py
@@ -41,9 +41,59 @@ Important classes and functions
 
 - PathEnembleFile : A writer of path ensemble data.
 """
+from __future__ import absolute_import
+import warnings
+# pyretis imports
 from .traj import create_traj_writer
 from .fileinout import FileWriter
 from .crossfile import CrossFile
 from .energyfile import EnergyFile
 from .orderfile import OrderFile
 from .pathfile import PathFile, PathEnsembleFile
+
+
+def get_file_object(file_type, file_name):
+    """Method to open a file with the correct file reader based on file type.
+
+    This is a convenience function to return an instance of `FileWriter` or
+    derived classes so that we are ready to read data from that file. Usage is
+    indended to be in cases when we just want to open a file easily.
+
+    Parameters
+    ----------
+    file_type : string
+        The desired file type
+    file_name : string
+        The file to open
+    ensemble : string
+        In case we are opening a path ensemble, we can specify the which one.
+    interfaces : list of floats
+        In case we are opening a path ensemble we can specify the interfaces
+
+    Returns
+    -------
+    out : object like `FileWriter` from `pyretis.inout.fileinout`
+
+    Examples
+    --------
+    >>> from pyretis.inout.fileinout import get_file_object
+    >>> crossfile = get_file_object('cross', 'cross.dat')
+    >>> print(crossfile)
+    """
+    if file_type == 'cross':
+        return CrossFile(file_name, mode='r')
+    elif file_type == 'order':
+        return OrderFile(file_name, mode='r')
+    elif file_type == 'energy':
+        return EnergyFile(file_name, mode='r')
+    elif file_type == 'pathensemble':
+        msg = 'Opening a path ensemble is not yet implemented completely!'
+        warnings.warn(msg)
+        return PathEnsembleFile(file_name, None, None, mode='r')
+    elif file_type == 'path':
+        return PathFile(file_name, mode='r')
+    else:
+        msg = 'Unknown file type {} requested. Aborting reading {}'
+        msg = msg.format(file_type, file_name)
+        warnings.warn(msg)
+        return None
