16b499308aecf1cc9cec3e1a6b7d5197e3411f4d
Anders Lervik <anders.lervik@ntnu.no>
2015-12-03 16:16:16 +0100

    Added function to check settings.

--- pyretis/inout/settings/common.py
+++ pyretis/inout/settings/common.py
@@ -3,6 +3,8 @@
 
 Important functions defined here:
 
+- check_settings: Check that required simulation settings are actually given.
+
 - import_from : A function to dynamically import functions/classes etc. from
   user specified modules.
 """
@@ -12,7 +14,7 @@ logger = logging.getLogger(__name__)  # pylint: disable=C0103
 logger.addHandler(logging.NullHandler())
 
 
-__all__ = ['import_from']
+__all__ = ['check_settings', 'import_from']
 
 
 def import_from(module_name, function_name):
@@ -44,3 +46,39 @@ def import_from(module_name, function_name):
     except ImportError:
         msg = 'Could not import module "{}"'.format(module_name)
         raise ValueError(msg)
+
+
+def check_settings(settings, required):
+    """Check that required simulation settings are actually given.
+
+    This function will look for required settings in the given
+    `settings`. If one or more keys from the given `required` list of
+    strings are not found, this function will return False. Otherwise
+    if will return True. Typically, and exception should be raised if False
+    is returned, this is handled outside the function in case someone wants to
+    add some magic handling of missing settings.
+
+    Parameters
+    ----------
+    settings : dict
+        This dict contains the given settings
+    required : list of strings
+        This list contains the settings that are required and which
+        we will check the presence of.
+
+    Returns
+    -------
+    result : boolean
+        True if all required settings are present, False otherwise.
+    not_found : list of strings
+        There are the required settings we did not find.
+    """
+    result = True
+    not_found = []
+    for setting in required:
+        if setting not in settings:
+            msg = 'Setting `{}` not found!'.format(setting)
+            logging.critical(msg)
+            result = False
+            not_found.append(setting)
+    return result, not_found
