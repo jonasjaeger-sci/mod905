c3a1c75603d98450a688dca16336bf59d8b91bad
Anders Lervik <anders.lervik@ntnu.no>
2016-01-13 16:44:23 +0100

    Updated parsing of input file.

--- pyretis/inout/settings/settings.py
+++ pyretis/inout/settings/settings.py
@@ -18,23 +18,24 @@ __all__ = ['parse_settings_file', 'parse_setting',
            'look_for_keyword', 'write_settings_file']
 
 
-KNOWN_KEYWORDS = {'integrator': 'dict',
+KNOWN_KEYWORDS = {'integrator': {'type': 'dict'},
                   #'orderparameter': 'many',
-                  'endcycle': 'number',
-                  'task': 'string',
-                  'units': 'dict',
+                  'endcycle': {'type': 'number'},
+                  'task': {'type': 'string'},
+                  'units': {'type': 'dict'},
                   #'ensemble': 'string',
-                  'interfaces': 'list',
+                  'interfaces': {'type': 'list'},
                   #'generate-vel': 'many',
-                  'output-dir': 'string',
-                  'box': 'dict',
-                  'particle-position': 'dict',
-                  'particle-velocity': 'dict',
-                  'dimensions': 'number',
-                  'temperature': 'number',
-                  'tis': 'dict',
-                  'particles-position': 'dict',
-                  'particles-velocity': 'dict'}
+                  'output-dir': {'type': 'string'},
+                  'box': {'type': 'dict'},
+                  'particle-position': {'type': 'dict'},
+                  'particle-velocity': {'type': 'dict'},
+                  'particle-mass': {'type': 'dict', 'append': True},
+                  'dimensions': {'type': 'number'},
+                  'temperature': {'type': 'number'},
+                  'tis': {'type': 'dict'},
+                  'particles-position': {'type': 'dict'},
+                  'particles-velocity': {'type': 'dict'}}
                   #a'output': 'many',
                   #'potentials': 'many',
                   #'potential-parameters': 'many',
@@ -96,29 +97,33 @@ def parse_setting(setting, keyword):
     parsed = None
     success = False
 
-    if keyword not in KNOWN_KEYWORDS:
-        return None, False
-
     try:
         str_setting = setting.split(' ', 1)[1].strip()
     except IndexError:
         return None, False
 
-    if KNOWN_KEYWORDS[keyword] == 'string':
+    key_type = KNOWN_KEYWORDS[keyword].get('type', '')
+
+    if key_type == 'string':
         parsed = str_setting
         success = True
-    elif KNOWN_KEYWORDS[keyword] == 'number':
+    elif key_type == 'number':
         try:
             parsed = ast.literal_eval(str_setting)
             success = True
         except SyntaxError:
             success = False
-    elif KNOWN_KEYWORDS[keyword] == 'dict':
+    elif key_type == 'dict':
         parsed = {}
         for opti in str_setting.split(','):
             key, _, val = opti.strip().partition(' ')
             key = key.strip().lower()
+            if len(key) < 1:
+                continue
             val = val.strip()
+            if len(val) < 1:
+                msg = 'Ignoring empty value: {} = {}'.format(key, val)
+                logger.warning(msg)
             try:
                 parsed[key] = ast.literal_eval(val)
             except SyntaxError:
@@ -146,26 +151,80 @@ def parse_settings_file(filename):
         A dictionary with settings for pyretis.
     """
     settings = {}
+    current_keyword = None
+    keyword_split = False
+    to_parse = []
     with open(filename, 'r') as fileh:
         for lines in fileh:
             to_read = lines.strip()
-            if not to_read:
+            if to_read.startswith('#') or not to_read:
+                if current_keyword is not None:
+                    parse_and_add(' '.join(to_parse), current_keyword,
+                                  settings)
+                    current_keyword = None
+                    keyword_split = False
+                    to_parse = []
                 continue
-            if to_read.startswith('#'):
-                continue
-            _, keyword, known = look_for_keyword(to_read)
-            if known:
-                parsed, success = parse_setting(to_read, keyword)
-                if success:
-                    if keyword in settings:
-                        msg = 'Updating already defined setting {}: {}'
-                        msgtxt = msg.format(keyword, settings[keyword])
-                        logger.warning(msgtxt)
-                    settings[keyword] = parsed
+            _, keyword, found_keyword = look_for_keyword(to_read)
+            if found_keyword:
+                if current_keyword is not None:
+                    parse_and_add(' '.join(to_parse), current_keyword,
+                                  settings)
+                keyword_split = to_read.endswith(',')
+                to_parse = []
+                if not keyword_split:
+                    # just parse this line
+                    parse_and_add(to_read, keyword, settings)
+                    current_keyword = None
                 else:
-                    msg = 'Could not understand setting "{}"'
-                    msgtxt = msg.format(to_read)
-                    logger.warning(msgtxt)
+                    current_keyword = keyword
+            if keyword_split:
+                to_parse.append(to_read)
+                keyword_split = to_read.endswith(',')
+                if not keyword_split:
+                    # parse things
+                    parse_and_add(' '.join(to_parse), current_keyword,
+                                  settings)
+                    current_keyword = None
+                    to_parse = []
+    return settings
+
+
+def parse_and_add(text, keyword, settings):
+    """Method that will parse a string and try to add the settings.
+
+    Parameters
+    ----------
+    text : string
+       The text to parse.
+    keyword : string
+        The keyword we are trying to parse data for.
+    settings : dict
+        Where to store the text.
+
+    Returns
+    -------
+    N/A but updates the input dictionary `settings`.
+    """
+    parsed, success = parse_setting(text, keyword)
+    if success:
+        append = KNOWN_KEYWORDS[keyword].get('append', False)
+        append = False
+        if append:
+            try:
+                settings[keyword].append(parsed)
+            except KeyError:
+                settings[keyword] = [parsed]
+        else:
+            if keyword in settings:
+                msg = 'Updating already defined setting {}: {}'
+                msgtxt = msg.format(keyword, settings[keyword])
+                logger.warning(msgtxt)
+            settings[keyword] = parsed
+    else:
+        msg = 'Could not understand setting "{}"'
+        msgtxt = msg.format(text)
+        logger.warning(msgtxt)
     return settings
 
 
