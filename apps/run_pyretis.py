50d302d823aa231ba1e9e467ed218065680800e4
Anders Lervik <anders.lervik@ntnu.no>
2016-01-18 13:51:56 +0100

    Update of run script to match new settings struct.

--- apps/run_pyretis.py
+++ apps/run_pyretis.py
@@ -2,14 +2,20 @@
 # -*- coding: utf-8 -*-
 """pyretis
 
-This script is a part of the pyretis library. It will parse input
-settings and run a simulation based on those settings.
+This script is a part of the pyretis library and can be used for
+running simulations from an input script.
+
+Usage is:
+
+run_pyretis -i inputfile.txt
 """
 # pylint: disable=C0103
 from __future__ import print_function, absolute_import
-import logging
 import argparse
+import datetime
+import logging
 import os
+# pyretis library imports:
 from pyretis import __version__ as VERSION
 from pyretis import __program_name__ as NAME
 from pyretis.core.units import create_conversion_factors
@@ -20,23 +26,71 @@ from pyretis.inout import create_output
 
 
 class MultiLineFormatter(logging.Formatter):
-    """Hardcoded formatter for pyretis log."""
+    """Hardcoded formatter for pyretis log file."""
     def format(self, record):
         out = logging.Formatter.format(self, record)
         shortname = record.name.split('.')[-1]
         out = out.replace(record.name, shortname)
         header, _ = out.split(record.message)
-        out = out.replace('\n', '\n' + ' '*len(header))
+        out = out.replace('\n', '\n' + ' ' * len(header))
         return out
 
 
-class MultiLineFormatter2(logging.Formatter):
-    """Hardcoded formatter for pyretis log."""
+class MultiLineFormatterDebug(logging.Formatter):
+    """Hardcoded formatter for pyretis log.
+
+    This formatter is intended for usage when more debugging
+    information is needed with a format for logging as:
+
+    `'%(name)s: [%(levelname)s]: %(message)s'`
+
+    so that information about modules will be printed out as well.
+    """
     def format(self, record):
         out = logging.Formatter.format(self, record)
         header, _ = out.split(record.message)
-        out = out.replace('\n', '\n' + ' '*len(header))
+        out = out.replace('\n', '\n' + ' ' * len(header))
         return out
+
+
+def hello_word(version, infile, basedir):
+    """Method to print out a standard greeting for pyretis.
+
+    Parameters
+    ----------
+    version : string
+        The version number of pyretis being used.
+    inputfile : string
+        String showing the location of the input file.
+    basedir : string
+        String showing the location we are running in.
+    """
+    timestart = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
+    msg = ['This is pyretis version {}'.format(version)]
+    msg += ['Start of pyretis execution: {}'.format(timestart)]
+    msg += ['Input file: {}'.format(infile)]
+    msg += ['Running in directory: {}'.format(basedir)]
+    msg += ['Please cite: [1] A. A., B. B., C. C, Some Journal, 99, pp. 100']
+    msgtxt = '\n'.join(msg)
+    print(msgtxt)
+    logger.info(msgtxt)
+
+
+def bye_bye_world():
+    """Method to print out the goodbye message for pyretis.
+
+    Parameters
+    ----------
+    version : string
+        String with the version number of pyretis.
+    """
+    timeend = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
+    msg = ['End of pyretis execution: {}'.format(timeend)]
+    msg += ['http://www.pyretis.org']
+    msgtxt = '\n'.join(msg)
+    print(msgtxt)
+    logger.info(msgtxt)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='pyretis')
@@ -60,15 +114,16 @@ if __name__ == '__main__':
     fileh.setLevel(logging.DEBUG)
     formatter = MultiLineFormatter('[%(levelname)s]: %(message)s')
     formatter_file = MultiLineFormatter('[%(levelname)s]: %(message)s')
-#formatter_file = MultiLineFormatter2('%(name)s: [%(levelname)s]: %(message)s')
     console.setFormatter(formatter)
     fileh.setFormatter(formatter_file)
     logger.addHandler(fileh)
     logger.addHandler(console)
+
+    hello_word(VERSION, localfile, basepath)
+
     settings = parse_settings_file(localfile)
-    units = settings['units']
-    settings['unit-system'] = units['system']
-    create_conversion_factors(settings['unit-system'])
+    create_conversion_factors(settings['units'], **settings['units-base'])
+
     system = create_system(settings)
     system.forcefield = create_force_field(settings)
     simulation = create_simulation(settings, system)
@@ -79,3 +134,4 @@ if __name__ == '__main__':
         #result['thermo']['stepno'] = stepno
         for task in output_tasks:
             task.output(result)
+    bye_bye_world()
