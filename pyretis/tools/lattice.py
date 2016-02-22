3b94a67c1149cc5908e139275c8780f657f0af91
Anders Lervik <anders.lervik@ntnu.no>
2016-02-22 14:38:54 +0100

    Added more robust error handling.

--- pyretis/tools/lattice.py
+++ pyretis/tools/lattice.py
@@ -7,7 +7,7 @@ initial structures on a lattice.
 Examples
 --------
 >>> from pyretis.tools.lattice import generate_lattice
->>> xyz = generate_lattice('diamond', [1, 1, 1], lcon=1)
+>>> xyz, size = generate_lattice('diamond', [1, 1, 1], lcon=1)
 """
 import itertools
 import numpy as np
@@ -69,15 +69,19 @@ def generate_lattice(lattice, repeat, lcon=None, density=None):
     try:
         unit_cell = UNIT_CELL[lattice.lower()]
     except KeyError:
-        msg = 'Unknown lattice "{}" requested!'.format(lattice)
-        raise ValueError(msg)
+        msgtxt = 'Unknown lattice "{}" requested!'.format(lattice)
+        raise ValueError(msgtxt)
+    except AttributeError:
+        msgtxt = ('Input lattice should '
+                  'be a string in {}'.format(UNIT_CELL.keys()))
+        raise ValueError(msgtxt)
     ndim = len(unit_cell[0])
     npart = len(unit_cell)
     if density is not None:
         lcon = (npart / density)**(1.0 / float(ndim))
     if lcon is None:
-        msg = 'Could not determine lattice constant!'
-        raise ValueError(msg)
+        msgtxt = 'Could not determine lattice constant!'
+        raise ValueError(msgtxt)
     positions = []
     if ndim == 2:
         nrx = repeat[0]
@@ -95,7 +99,7 @@ def generate_lattice(lattice, repeat, lcon=None, density=None):
             positions.extend(pos)
         size = [[0.0, i * lcon] for i in (nrx, nry, nrz)]
     else:
-        msg = 'Can not create lattices with dimensionality "{}".'
-        raise ValueError(msg.format(ndim))
+        msgtxt = 'Can not create lattices with dimensionality "{}".'
+        raise ValueError(msgtxt.format(ndim))
     positions = np.array(positions)
     return positions, size
