b4ea34690fcf5f625311311a77413ff599374e08
Anders Lervik <andersle@gmail.com>
2016-02-24 14:15:25 +0100

    Fixing some docstrings.

--- pyretis/__init__.py
+++ pyretis/__init__.py
@@ -21,13 +21,16 @@ Sub-Packages:
   event simulations. This includes classes defining the system,
   a system box, simulations etc.
 
-- forcefield: This package define forcefields and how they are calculated.
+- forcefield: This package define forcefields and how they are
+  calculated.
 
 - tools: This package defines some functions which can be useful for
-  setting up simple systems, for example functions for generating lattices.
+  setting up simple systems, for example functions for generating
+  lattices.
 
-- inout: This package defines the io for the pyretis program. This includes
-  generating output from the analysis and reading input-files etc.
+- inout: This package defines the io for the pyretis program.
+  This includes generating output from the analysis and reading
+  input-files etc.
 
 - analysis: This package defines the analysis tools for calculating
   crossing probabilities, rates etc.
