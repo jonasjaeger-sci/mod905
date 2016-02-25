19c0db18992ef2cdccc15bb74e3fbb5fe55a1b56
Anders Lervik <anders.lervik@ntnu.no>
2016-02-25 17:12:56 +0100

    Added example using fortran for integration
    This example just shows how fortran can be used in the numerical
    integration for a very simple example -> the Velocity Verlet
    integrator.

---
+++ examples/integrators/fortran/README.rst
@@ -0,0 +1,11 @@
+Extending pyretis with Fortran
+==============================
+
+This folder contains an example of extending pyretis with a new
+integrator which is implemented in Fortran.
+
+The Fortran code must be compiled before it can be executed and this
+is done by running `make`.
+
+Note that you may have to edit the makefile depending on your version
+of python. For python3 change `f2py` to `f2py3` in the makefile.
---
+++ examples/integrators/fortran/vvintegrator.f90
@@ -0,0 +1,43 @@
+module vvintegrator
+
+implicit none
+
+private
+
+public :: step1, step2
+
+contains
+
+subroutine step1(pos, vel, force, imass, delta_t, half_delta_t, n, d, dpos, dvel) 
+! Subroutine to evaluate the potential
+implicit none
+integer, intent(in) :: n, d
+double precision, dimension(n, d), intent(in) :: pos, vel, force
+double precision, dimension(n), intent(in) :: imass
+double precision, intent(in) :: delta_t, half_delta_t
+double precision, dimension(n, d), intent(out) :: dvel, dpos
+integer :: i
+dvel = 0.0D0
+dpos = 0.0D0
+do i=1,n
+    dvel(i,:) = vel(i,:) + half_delta_t * force(i,:) * imass(i)
+    dpos(i,:) = pos(i,:) + delta_t * dvel(i,:)
+end do
+end subroutine step1
+
+subroutine step2(vel, force, imass, half_delta_t, n, d, dvel) 
+! Subroutine to evaluate the potential
+implicit none
+integer, intent(in) :: n, d
+double precision, dimension(n, d), intent(in) :: vel, force
+double precision, dimension(n), intent(in) :: imass
+double precision, intent(in) :: half_delta_t
+double precision, dimension(n, d), intent(out) :: dvel
+integer :: i
+dvel = 0.0D0
+do i=1,n
+    dvel(i,:) = vel(i,:) + half_delta_t * force(i,:) * imass(i)
+end do
+end subroutine step2
+
+end module vvintegrator
