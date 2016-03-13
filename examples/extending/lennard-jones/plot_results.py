6362e23e3ff43a5e01af4bd4fd550feb2ae96637
Anders Lervik <anders.lervik@ntnu.no>
2016-03-13 11:41:06 +0100

    Added scripts and readme
    Script will plot some results and time python and numpy implementations.

---
+++ examples/extending/lennard-jones/plot_results.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""Plot results from time tests of the Lennard-Jones potential.
+
+Here we assume that the results are available in files named
+``timeings.txt``.
+"""
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pylab as plt
+
+
+mpl.rcParams['axes.color_cycle'] = ['4C72B0', '55A868', 'C44E52', '8172B2',
+                                    'CCB974', '64B5CD']
+mpl.rcParams['axes.labelsize'] = 'medium'
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.serif'] = 'Times'
+mpl.rcParams['font.size'] = 16
+mpl.rcParams['legend.numpoints'] = 1
+mpl.rcParams['legend.scatterpoints'] = 1
+mpl.rcParams['legend.handlelength'] = 1.0
+mpl.rcParams['legend.handleheight'] = 0.3
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['xtick.major.pad'] = 6
+mpl.rcParams['xtick.labelsize'] = 'medium'
+mpl.rcParams['ytick.major.pad'] = 6
+mpl.rcParams['ytick.labelsize'] = 'medium'
+
+results = {'c': 'c/timings.txt',
+           'c (python3)': 'c-python3/timings.txt',
+           'fortran': 'fortran/timings.txt',
+           'fortran (pointer)': 'fortran-pointer/timings.txt',
+           'numpy': 'timings-numpy.txt'}
+
+markers = ['o', 'v', '^', 's', '*', 'x', 'p', 'h', '+']
+
+data = {}
+for key in results:
+    data[key] = np.loadtxt(results[key])
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+for i, key in enumerate(data):
+    result = data[key]
+    npart, avg, std = result[:, 0], result[:, 2], result[:, 3]
+    ax1.errorbar(npart, avg, yerr=std, label=key, lw=3, markersize=9,
+                 marker=markers[i], alpha=0.9)
+ax1.legend(ncol=2)
+
+ax1.set_xlabel('System size')
+ax1.set_ylabel('Time')
+fig.tight_layout()
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+norm_avg = data['c'][:, 2]
+
+for i, key in enumerate(data):
+    result = data[key]
+    npart, avg = result[:, 0], result[:, 2]
+    nmin = min(len(norm_avg), len(avg))
+    avg_n = avg[:nmin] / norm_avg[:nmin]
+    ax2.plot(npart, avg_n, lw=3, label=key, markersize=9, alpha=0.9,
+             marker=markers[i])
+ax2.legend(ncol=2)
+ax2.set_yscale('log')
+ax2.set_xlabel('System size')
+ax2.set_ylabel('Time relative to python+c')
+fig2.tight_layout()
+plt.show()
