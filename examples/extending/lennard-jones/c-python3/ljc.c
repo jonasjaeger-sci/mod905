303373b2968ed23e5308f5ff5c36be166559d74e
Anders Lervik <anders.lervik@ntnu.no>
2016-03-13 11:09:57 +0100

    Added example for python3 + c

---
+++ examples/extending/lennard-jones/c-python3/ljc.c
@@ -0,0 +1,304 @@
+
+#include <Python.h>
+#include <numpy/arrayobject.h>
+#include <math.h>
+//#include <stdio.h>  // printf
+
+// Forward function declaration.
+static PyObject *potential(PyObject *self, PyObject *args); 
+static PyObject *force(PyObject *self, PyObject *args); 
+static PyObject *force_and_pot(PyObject *self, PyObject *args); 
+
+// Boilerplate: function list.
+static PyMethodDef ljmethods[] = {
+  { "potential", potential, METH_VARARGS, "Calculate potential energy."},
+  { "force", force, METH_VARARGS, "Calculate force."},
+  { "force_and_pot", force_and_pot, METH_VARARGS, "Calculate force and potential energy."},
+  { NULL, NULL, 0, NULL } /* Sentinel */
+};
+
+static struct PyModuleDef ljc =
+{
+    PyModuleDef_HEAD_INIT,
+    "ljc",
+    "Compute Lennard-Jones interactions",
+    -1,
+    ljmethods
+};
+
+// Boilerplate: Module initialization.
+PyMODINIT_FUNC PyInit_ljc(void) {
+  import_array();
+  return PyModule_Create(&ljc);
+}
+
+
+static inline npy_float64 pbc_dist(npy_float64 x,
+                                   npy_float64 box,
+                                   npy_float64 ibox) {
+    return x - nearbyint(x * ibox) * box;
+}
+
+
+static PyObject *potential(PyObject *self, PyObject *args) {
+  // Input variables:
+  npy_int64 npart, dim, ntype;
+  npy_int64 itype, loc;
+  PyArrayObject *py_pos, *py_box, *py_ibox, *py_lj3, *py_lj4, *py_rcut2, *py_offset, *py_ptype;
+  // Internal variables:
+  npy_float64 dx, dy, dz, rsq, r2inv, r6inv;
+  npy_int64 i, xi, yi, zi;
+  npy_int64 j, xj, yj, zj;
+
+  // Parse arguments. 
+  if (!PyArg_ParseTuple(args, "O!O!O!O!O!O!O!O!lll",
+                        &PyArray_Type, &py_pos,
+                        &PyArray_Type, &py_box,
+                        &PyArray_Type, &py_ibox,
+                        &PyArray_Type, &py_lj3,
+                        &PyArray_Type, &py_lj4,
+                        &PyArray_Type, &py_offset,
+                        &PyArray_Type, &py_rcut2,
+                        &PyArray_Type, &py_ptype,
+                        &npart, &dim, &ntype)){
+    return NULL;
+  }
+  // Get underlying arrays from numpy arrays. 
+  npy_float64 *pos = (npy_float64*)PyArray_DATA(py_pos);
+  npy_float64 *box = (npy_float64*)PyArray_DATA(py_box);
+  npy_float64 *ibox = (npy_float64*)PyArray_DATA(py_ibox);
+  npy_float64 *lj3 = (npy_float64*)PyArray_DATA(py_lj3);
+  npy_float64 *lj4 = (npy_float64*)PyArray_DATA(py_lj4);
+  npy_float64 *rcut2 = (npy_float64*)PyArray_DATA(py_rcut2);
+  npy_float64 *offset = (npy_float64*)PyArray_DATA(py_offset);
+  npy_int64 *ptype = (npy_int64*)PyArray_DATA(py_ptype);
+  npy_float64 vpot = 0.0;
+  // Calculate potential:
+  for(i = 0; i < npart-1; i++){
+    xi = 3*i;
+    yi = 3*i + 1;
+    zi = 3*i + 2;
+    itype = ptype[i];
+    for (j = i + 1; j < npart; j++){
+        loc = itype + ntype * ptype[j];
+        xj = 3*j;
+        yj = 3*j + 1;
+        zj = 3*j + 2;
+        dx = pos[xi] - pos[xj];
+        dy = pos[yi] - pos[yj];
+        dz = pos[zi] - pos[zj];
+        dx = pbc_dist(dx, box[0], ibox[0]);
+        dy = pbc_dist(dy, box[1], ibox[1]);
+        dz = pbc_dist(dz, box[2], ibox[2]);
+        rsq = dx*dx + dy*dy + dz*dz;
+        if (rsq < rcut2[loc]){
+            r2inv = 1.0 / rsq;
+            r6inv = r2inv*r2inv*r2inv;
+            vpot += r6inv * (lj3[loc] * r6inv - lj4[loc]) - offset[loc];
+        }
+    }
+  }
+  /* Clean up */
+  /* Build the output tuple */
+  PyObject *ret = Py_BuildValue("d", vpot);
+  return ret;
+}
+
+
+static PyObject *force(PyObject *self, PyObject *args) {
+  // Input variables:
+  npy_int64 npart, dim, ntype;
+  npy_int64 itype, loc;
+  PyArrayObject *py_pos, *py_box, *py_ibox, *py_force, *py_virial;
+  PyArrayObject *py_lj1, *py_lj2, *py_rcut2, *py_ptype;
+  // Internal variables:
+  npy_float64 dx, dy, dz, rsq, r2inv, r6inv;
+  npy_float64 forcelj, fx, fy, fz;
+  npy_int64 i, xi, yi, zi;
+  npy_int64 j, xj, yj, zj;
+
+  // Parse arguments. 
+  if (!PyArg_ParseTuple(args, "O!O!O!O!O!O!O!O!O!lll",
+                        &PyArray_Type, &py_pos,
+                        &PyArray_Type, &py_box,
+                        &PyArray_Type, &py_ibox,
+                        &PyArray_Type, &py_lj1,
+                        &PyArray_Type, &py_lj2,
+                        &PyArray_Type, &py_rcut2,
+                        &PyArray_Type, &py_ptype,
+                        &PyArray_Type, &py_force,
+                        &PyArray_Type, &py_virial,
+                        &npart, &dim, &ntype)){
+    return NULL;
+  }
+  // Get underlying arrays from numpy arrays. 
+  npy_float64 *force = (npy_float64*)PyArray_DATA(py_force);
+  npy_float64 *virial = (npy_float64*)PyArray_DATA(py_virial);
+  npy_float64 *pos = (npy_float64*)PyArray_DATA(py_pos);
+  npy_float64 *box = (npy_float64*)PyArray_DATA(py_box);
+  npy_float64 *ibox = (npy_float64*)PyArray_DATA(py_ibox);
+  npy_float64 *lj1 = (npy_float64*)PyArray_DATA(py_lj1);
+  npy_float64 *lj2 = (npy_float64*)PyArray_DATA(py_lj2);
+  npy_float64 *rcut2 = (npy_float64*)PyArray_DATA(py_rcut2);
+  npy_int64 *ptype = (npy_int64*)PyArray_DATA(py_ptype);
+  // Create array for forces:
+  //PyObject *ret = PyArray_SimpleNew(dim, test, NPY_DOUBLE);
+  // Zero virial:
+  for(i = 0; i < dim*dim; i++) {
+    virial[i] = 0;
+  }
+  // Set all forces to zero. 
+  for(i = 0; i < npart; i++) {
+    force[3*i] = 0;
+    force[3*i+1] = 0;
+    force[3*i+2] = 0;
+  }
+  // Calculate forces:
+  for(i = 0; i < npart-1; i++){
+    xi = 3*i;
+    yi = 3*i + 1;
+    zi = 3*i + 2;
+    itype = ptype[i];
+    for (j = i + 1; j < npart; j++){
+        loc = itype + ntype * ptype[j];
+        xj = 3*j;
+        yj = 3*j + 1;
+        zj = 3*j + 2;
+        dx = pos[xi] - pos[xj];
+        dy = pos[yi] - pos[yj];
+        dz = pos[zi] - pos[zj];
+        dx = pbc_dist(dx, box[0], ibox[0]);
+        dy = pbc_dist(dy, box[1], ibox[1]);
+        dz = pbc_dist(dz, box[2], ibox[2]);
+        rsq = dx*dx + dy*dy + dz*dz;
+        if (rsq < rcut2[loc]){
+            r2inv = 1.0 / rsq;
+            r6inv = r2inv*r2inv*r2inv;
+            forcelj = r2inv * r6inv * (lj1[loc] * r6inv - lj2[loc]);
+            fx = forcelj * dx;
+            fy = forcelj * dy;
+            fz = forcelj * dz;
+            force[xi] += fx;
+            force[yi] += fy;
+            force[zi] += fz;
+            force[xj] -= fx;
+            force[yj] -= fy;
+            force[zj] -= fz;
+            virial[0] += fx*dx;
+            virial[1] += fx*dy;
+            virial[2] += fx*dz;
+            virial[3] += fy*dx;
+            virial[4] += fy*dy;
+            virial[5] += fy*dz;
+            virial[6] += fz*dx;
+            virial[7] += fz*dy;
+            virial[8] += fz*dz;
+        }
+    }
+  }
+  Py_RETURN_NONE;
+}
+
+
+static PyObject *force_and_pot(PyObject *self, PyObject *args) {
+  // Input variables:
+  npy_int64 npart, dim, ntype;
+  npy_int64 itype, loc;
+  PyArrayObject *py_pos, *py_box, *py_ibox, *py_force, *py_virial;
+  PyArrayObject *py_lj1, *py_lj2, *py_lj3, *py_lj4, *py_rcut2, *py_offset, *py_ptype;
+  // Internal variables:
+  npy_float64 dx, dy, dz, rsq, r2inv, r6inv;
+  npy_float64 forcelj, fx, fy, fz;
+  npy_int64 i, xi, yi, zi;
+  npy_int64 j, xj, yj, zj;
+
+  // Parse arguments. 
+  if (!PyArg_ParseTuple(args, "O!O!O!O!O!O!O!O!O!O!O!O!lll",
+                        &PyArray_Type, &py_pos,
+                        &PyArray_Type, &py_box,
+                        &PyArray_Type, &py_ibox,
+                        &PyArray_Type, &py_lj1,
+                        &PyArray_Type, &py_lj2,
+                        &PyArray_Type, &py_lj3,
+                        &PyArray_Type, &py_lj4,
+                        &PyArray_Type, &py_offset,
+                        &PyArray_Type, &py_rcut2,
+                        &PyArray_Type, &py_ptype,
+                        &PyArray_Type, &py_force,
+                        &PyArray_Type, &py_virial,
+                        &npart, &dim, &ntype)){
+    return NULL;
+  }
+  // Get underlying arrays from numpy arrays. 
+  npy_float64 *force = (npy_float64*)PyArray_DATA(py_force);
+  npy_float64 *virial = (npy_float64*)PyArray_DATA(py_virial);
+  npy_float64 *pos = (npy_float64*)PyArray_DATA(py_pos);
+  npy_float64 *box = (npy_float64*)PyArray_DATA(py_box);
+  npy_float64 *ibox = (npy_float64*)PyArray_DATA(py_ibox);
+  npy_float64 *lj1 = (npy_float64*)PyArray_DATA(py_lj1);
+  npy_float64 *lj2 = (npy_float64*)PyArray_DATA(py_lj2);
+  npy_float64 *lj3 = (npy_float64*)PyArray_DATA(py_lj3);
+  npy_float64 *lj4 = (npy_float64*)PyArray_DATA(py_lj4);
+  npy_float64 *offset = (npy_float64*)PyArray_DATA(py_offset);
+  npy_float64 *rcut2 = (npy_float64*)PyArray_DATA(py_rcut2);
+  npy_int64 *ptype = (npy_int64*)PyArray_DATA(py_ptype);
+  npy_float64 vpot = 0.0;
+  // Create array for forces:
+  //PyObject *ret = PyArray_SimpleNew(dim, test, NPY_DOUBLE);
+  // Zero virial:
+  for(i = 0; i < dim*dim; i++) {
+    virial[i] = 0;
+  }
+  // Set all forces to zero. 
+  for(i = 0; i < npart; i++) {
+    force[3*i] = 0;
+    force[3*i+1] = 0;
+    force[3*i+2] = 0;
+  }
+  // Calculate forces:
+  for(i = 0; i < npart-1; i++){
+    xi = 3*i;
+    yi = 3*i + 1;
+    zi = 3*i + 2;
+    itype = ptype[i];
+    for (j = i + 1; j < npart; j++){
+        loc = itype + ntype * ptype[j];
+        xj = 3*j;
+        yj = 3*j + 1;
+        zj = 3*j + 2;
+        dx = pos[xi] - pos[xj];
+        dy = pos[yi] - pos[yj];
+        dz = pos[zi] - pos[zj];
+        dx = pbc_dist(dx, box[0], ibox[0]);
+        dy = pbc_dist(dy, box[1], ibox[1]);
+        dz = pbc_dist(dz, box[2], ibox[2]);
+        rsq = dx*dx + dy*dy + dz*dz;
+        if (rsq < rcut2[loc]){
+            r2inv = 1.0 / rsq;
+            r6inv = r2inv*r2inv*r2inv;
+            forcelj = r2inv * r6inv * (lj1[loc] * r6inv - lj2[loc]);
+            vpot += r6inv * (lj3[loc] * r6inv - lj4[loc]) - offset[loc];
+            fx = forcelj * dx;
+            fy = forcelj * dy;
+            fz = forcelj * dz;
+            force[xi] += fx;
+            force[yi] += fy;
+            force[zi] += fz;
+            force[xj] -= fx;
+            force[yj] -= fy;
+            force[zj] -= fz;
+            virial[0] += fx*dx;
+            virial[1] += fx*dy;
+            virial[2] += fx*dz;
+            virial[3] += fy*dx;
+            virial[4] += fy*dy;
+            virial[5] += fy*dz;
+            virial[6] += fz*dx;
+            virial[7] += fz*dy;
+            virial[8] += fz*dz;
+        }
+    }
+  }
+  PyObject *ret = Py_BuildValue("d", vpot);
+  return ret;
+}
---
+++ examples/extending/lennard-jones/c-python3/ljpotentialc.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""Example of using a Lennard-Jones potential implemented in C."""
+import logging
+import numpy as np
+logger = logging.getLogger(__name__)  # pylint: disable=C0103
+logger.addHandler(logging.NullHandler())
+# pyretis imports
+from pyretis.forcefield.potentials import PairLennardJonesCut
+from pyretis.forcefield.potentials.pairpotentials import generate_pair_interactions
+try:
+    import ljc
+except ImportError:
+    MSG = ('Could not import external C library.'
+           '\nPlease compile with: "python setup.py build_ext --inplace"')
+    logger.critical(MSG)
+    raise ImportError(MSG)
+
+
+
+__all__ = ['PairLennardJonesCutC']
+
+
+class PairLennardJonesCutC(PairLennardJonesCut):
+    r"""class PairLennardJonesCutC(PairLennardJonesCut).
+
+    This class implements as simple Lennard-Jones 6-12 potential which
+    employs a simple cut-off and can be shifted. The potential energy
+    (:math:`V_\text{pot}`) is defined in the usual way for an
+    interacting pair of particles a distance :math:`r` apart,
+
+    .. math::
+
+       V_\text{pot} = 4 \varepsilon \left( x^{12} - x^{6} \right),
+
+    where :math:`x = \sigma/r` and :math:`\varepsilon`
+    and :math:`\sigma` are the potential parameters. The parameters are
+    stored as attributes of the potential and we store one set for each
+    kind of pair interaction. Parameters can be generated with a
+    specific mixing rule by the force field.
+
+    Attributes
+    ----------
+    params : dict
+        The parameters for the potential. This dict is assumed to
+        contain parameters for pairs, i.e. for interactions.
+    _lj1 : numpy.array
+        Lennard-Jones parameters used for calculation of the force.
+        Keys are the pairs (particle types) that may interact.
+        Calculated as: ``48.0 * epsilon * sigma**12``
+    _lj2 : numpy.array
+        Lennard-Jones parameters used for calculation of the force.
+        Keys are the pairs (particle types) that may interact.
+        Calculated as: ``24.0 * epsilon * sigma**6``
+    _lj3 : numpy.array
+        Lennard-Jones parameters used for calculation of the potential.
+        Keys are the pairs (particle types) that may interact.
+        Calculated as: ``4.0 * epsilon * sigma**12``
+    _lj4 : numpy.array
+        Lennard-Jones parameters used for calculation of the potential.
+        Keys are the pairs (particle types) that may interact.
+        Calculated as: ``4.0 * epsilon * sigma**6``
+    _offset : numpy.array
+        Potential values for shifting the potential if requested.
+        This is the potential evaluated at the cutoff.
+    _rcut2 : numpy.array
+        Squared cut-off for each interaction type.
+        Keys are the pairs (particle types) that may interact.
+    """
+
+    def __init__(self, dim=3, shift=True,
+                 desc='Lennard-Jones pair potential (C)'):
+        """Initiate the Lennard-Jones potential.
+
+        Parameters
+        ----------
+        dim : int
+            The dimensionality to use.
+        shift : boolean
+            Determines if the potential should be shifted or not.
+        """
+        super(PairLennardJonesCutC, self).__init__(dim=dim, desc=desc)
+        self.ntype = 0
+
+    def set_parameters(self, parameters):
+        """Update all parameters.
+
+        Here, we generate pair interactions, since that is what this
+        potential actually is using.
+
+        Parameters
+        ----------
+        parameters : dict
+            The input base parameters
+        """
+        self.params = {}
+        pair_param = generate_pair_interactions(parameters)
+        self.ntype = max(int(np.sqrt(len(pair_param))), 2)
+        self._lj1 = np.zeros((self.ntype, self.ntype))
+        self._lj2 = np.zeros_like(self._lj1)
+        self._lj3 = np.zeros_like(self._lj1)
+        self._lj4 = np.zeros_like(self._lj1)
+        self._rcut2 = np.zeros_like(self._lj1)
+        self._offset = np.zeros_like(self._lj1)
+        for pair in pair_param:
+            eps_ij = pair_param[pair]['epsilon']
+            sig_ij = pair_param[pair]['sigma']
+            rcut = pair_param[pair]['rcut']
+            self._lj1[pair] = 48.0 * eps_ij * sig_ij**12
+            self._lj2[pair] = 24.0 * eps_ij * sig_ij**6
+            self._lj3[pair] = 4.0 * eps_ij * sig_ij**12
+            self._lj4[pair] = 4.0 * eps_ij * sig_ij**6
+            self._rcut2[pair] = rcut**2
+            vcut = 0.0
+            if self.shift:
+                try:
+                    vcut = 4.0 * eps_ij * ((sig_ij / rcut)**12 -
+                                           (sig_ij / rcut)**6)
+                except ZeroDivisionError:
+                    vcut = 0.0
+            self._offset[pair] = vcut
+            self.params[pair] = pair_param[pair]
+
+    def potential(self, particles, box):
+        """Calculate the potential energy for the Lennard-Jones interaction.
+
+        Parameters
+        ----------
+        particles : object like `Particles` from `pyretis.core.particles`
+            The particle list.
+        box : object like `Box` from `pyretis.core.box`
+            Representation of the box used in the simulation.
+
+        Returns
+        -------
+        The potential energy as a float.
+        """
+        v_pot = ljc.potential(particles.pos,
+                              box.length, box.ilength,
+                              self._lj3, self._lj4, self._offset,
+                              self._rcut2, particles.ptype,
+                              particles.npart,
+                              box.dim, self.ntype)
+        return v_pot
+
+    def force(self, particles, box):
+        """Calculate the force for the Lennard-Jones interaction.
+
+        We also calculate the virial here, since the force
+        is evaluated.
+
+        Parameters
+        ----------
+        particles : object like `Particles` from `pyretis.core.particles`
+            The particle list.
+        box : object like `Box` from `pyretis.core.box`
+            Representation of the box used in the simulation.
+
+        Returns
+        -------
+        The force as a numpy.array of the same shape as the positions
+        in `particles.pos`.
+        """
+        forces = np.zeros((particles.npart, box.dim))
+        virial = np.zeros((box.dim, box.dim))
+        ljc.force(particles.pos,
+                  box.length, box.ilength,
+                  self._lj1, self._lj2, self._rcut2,
+                  particles.ptype,
+                  forces, virial,
+                  particles.npart,
+                  box.dim, self.ntype)
+        return forces, virial
+
+    def potential_and_force(self, particles, box):
+        """Calculate potential and force for the Lennard-Jones interaction.
+
+        Since the force is evaluated, the virial is also calculated.
+
+        Parameters
+        ----------
+        particles : object like `Particles` from `pyretis.core.particles`
+            The particle list.
+        box : object like `Box` from `pyretis.core.box`
+            Representation of the box used in the simulation.
+
+        Note
+        ----
+        Currently, the virial is only calculated for the particles as a
+        whole. It is not calculated as a virial per atom. The virial
+        per atom might be useful to obtain a local pressure or stress,
+        however this needs some consideration. Perhaps it's best to
+        fully implement this as a method of planes or something similar.
+
+        Returns
+        -------
+        out[0] : float
+            The potential energy as a float.
+        out[1] : numpy.array
+            The force as a numpy.array of the same shape as the
+            positions in `particles.pos`.
+        out[2] : numpy.array
+            The virial, as a symmetric matrix with dimensions
+            (dim, dim) where dim is given by the box/system dimensions.
+        """
+        forces = np.zeros((particles.npart, box.dim))
+        virial = np.zeros((box.dim, box.dim))
+        vpot = ljc.force_and_pot(particles.pos,
+                                 box.length,
+                                 box.ilength,
+                                 self._lj1,
+                                 self._lj2,
+                                 self._lj3,
+                                 self._lj4,
+                                 self._offset,
+                                 self._rcut2,
+                                 particles.ptype,
+                                 forces,
+                                 virial,
+                                 particles.npart,
+                                 box.dim,
+                                 self.ntype)
+        return vpot, forces, virial
