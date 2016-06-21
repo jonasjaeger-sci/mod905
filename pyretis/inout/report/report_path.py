d4c0eb092355dddb32aa314c767b3ae6ed305b6f
Anders Lervik <anders.lervik@ntnu.no>
2016-06-21 15:14:10 +0200

    Updated the documentation.

--- pyretis/inout/report/report_path.py
+++ pyretis/inout/report/report_path.py
@@ -29,15 +29,7 @@ def generate_report_tis_path(analysis, output='rst'):
     Parameters
     ----------
     analysis : dict
-        This is the output (and some input) for the analysis.
-        The keys are:
-
-        * `tis-fig`: list of corresponding figures (to 'tis').
-        * `detect`: locations of the interfaces used for detection.
-        * `ensemble`: The name of the path ensemble we are reporting
-          results for.
-        * `interfaces`: list with the interfaces used for this path
-          ensemble.
+        This is the output from the analysis.
     output : string, optional
         This is the desired output format. It must match one of the
         formats defined in `.report._TEMPLATES`. Default is 'rst'
@@ -75,20 +67,7 @@ def generate_report_tis(analysis, output='rst'):
     Parameters
     ----------
     analysis : dict
-        This is the output (and some input!) for the analysis. The keys
-        we make use of are:
-
-        * `tis`: list of dicts with the results from analysing the path
-          ensembles. `analysis['tis'][i]` contains the analysis results
-          for path ensemble no. `i`.
-        * `tis-fig`: list of corresponding figures (to 'tis').
-        * `matched`: results from the matching of probability.
-        * `matched-fig`: the figure corresponding to 'matched'.
-        * `detect`: locations of the interfaces used for detection.
-        * `ensembles`: list of strings with the names of the path
-          ensembles we are reporting results for.
-        * `interfaces`: list of lists which give the interfaces for the
-          different ensembles.
+        This is the output from the analysis.
     output : string, optional
         This is the desired output format. It must match one of the
         formats defined in `.report._TEMPLATES`. Default is 'rst'
