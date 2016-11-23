c868b310f6e09377169e12bcf15470337c04ee96
Anders Lervik <andersle@gmail.com>
2016-11-23 22:02:00 +0100

    [WIP] Files for 2D hysteresis.

---
+++ examples/path_sampling/2D-hysteresis/retis-xy/orderp.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, pyretis Development Team.
+# Distributed under the GPLV3 License. See LICENSE for more info.
+"""This file defines the order parameter used for the WCA example.
+"""
+import logging
+import numpy as np
+from pyretis.orderparameter import OrderParameter
+logger = logging.getLogger(__name__)  # pylint: disable=C0103
+logger.addHandler(logging.NullHandler())
+
+
+class OrderXY(OrderParameter):
+    """OrderXY(OrderParameter).
+
+    This class defines a 2D order parameter such that x + y = constant.
+
+    Attributes
+    ----------
+    name : string
+        A human readable name for the order parameter
+    index : integer
+        This selects the particle to use for the order parameter.
+    periodic : boolean
+        This determines if periodic boundaries should be applied to
+        the position or not.
+    """
+
+    def __init__(self, index):
+        """Initialize the order parameter.
+
+        Parameters
+        ----------
+        name : string
+            The name for the order parameter
+        index : tuple of ints
+            This is the indices of the atom we will use the position of.
+        periodic : boolean, optional
+            This determines if periodic boundary conditions should be
+            applied to the position.
+        """
+        super(OrderXY, self).__init__('order', desc='2D->1D projection')
+        self.index = index
+        x1 = 0.2
+        y1 = 0.4
+        x0 = -0.2
+        y0 = -0.4
+        self.x0 = x0
+        self.y0 = y0
+        self.origin = np.array([x0, y0])
+        self.vec = np.array([x1 - x0, y1 - y0])
+        self.vec /= np.sqrt(np.dot(self.vec, self.vec))
+
+    def calculate(self, system):
+        """Calculate the order parameter.
+
+        Here, the order parameter is just the distance between two
+        particles.
+
+        Parameters
+        ----------
+        system : object like `System` from `pyretis.core.system`
+            This object is used for the actual calculation, typically
+            only `system.particles.pos` and/or `system.particles.vel`
+            will be used. In some cases `system.forcefield` can also be
+            used to include specific energies for the order parameter.
+
+        Returns
+        -------
+        out : float
+            The order parameter.
+        """
+        pos = system.particles.pos[self.index]
+        x = pos[0]
+        y = pos[1]
+        vec = np.array([x - self.x0, y - self.y0])
+        proj = np.dot(vec, self.vec)
+        return proj
+
+    def calculate_velocity(self, system):
+        """Calculate the time derivative of the order parameter.
+
+        For this order parameter it is given by the time derivative of
+        the distance vector.
+
+        Parameters
+        ----------
+        system : object like `System` from `pyretis.core.system`
+            This object is used for the actual calculation.
+
+        Returns
+        -------
+        out : float
+            The velocity of the order parameter
+        """
+        return None
---
+++ examples/path_sampling/2D-hysteresis/retis-y/retis.rst
@@ -0,0 +1,81 @@
+Retis 1D example
+================
+
+Simulation
+----------
+task = retis
+steps = 20000
+interfaces = [-0.32, -0.21, -0.13, -0.08, -0.03, 0.03, 0.10, 0.20]
+
+System
+------
+units = reduced
+dimensions = 2
+temperature = 0.4
+
+Box
+---
+periodic = [False, False]
+
+Integrator
+----------
+class = Langevin
+timestep = 0.002
+gamma = 0.3
+high_friction = False
+seed = 0
+
+TIS settings
+------------
+freq =  0.5
+maxlength = 20000
+aimless = True
+allowmaxlength = False
+zero_momentum = False
+rescale_energy = False
+sigma_v =  -1
+seed = 0
+initial_path = kick
+
+RETIS settings
+--------------
+swapfreq = 0.5
+relative_shoots = None
+nullmoves = True
+swapsimul = True
+
+Particles
+---------
+position = {'file': '../initial.xyz'}
+velocity = {'generate': 'maxwell',
+            'momentum': False,
+            'seed': 0}
+mass = {'Ar': 1.0}
+name = ['Ar']
+type = [0]
+
+Forcefield settings
+-------------------
+description = 2D hysteresis
+
+Potential
+---------
+class = Hyst2D
+module = ../potential.py
+parameter gamma1 = 1
+parameter gamma2 = -10
+parameter gamma3 = -10
+parameter alpha1 = -30
+parameter alpha2 = -3
+parameter beta1 = -30
+parameter beta2 = -3
+parameter x0 = 0.2
+parameter y0 = 0.4
+
+Orderparameter
+--------------
+class = Position
+dim = y
+index = 0
+name = Position
+periodic = False
