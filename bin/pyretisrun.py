f0cc37ddcfd1bfb67d41392ce7c75525487ee79d
Anders Lervik <anders.lervik@ntnu.no>
2017-02-16 14:39:58 +0100

    Fixed printing of None to log-file.

--- bin/pyretisrun.py
+++ bin/pyretisrun.py
@@ -95,17 +95,18 @@ def hello_world(infile, rundir, logfile):
     msgtxt += [r"| |_) || |_| || |  |  __/| |_ | |\__ \ "]
     msgtxt += [r"| .__/  \__, ||_|   \___| \__||_||___/"]
     msgtxt += [r"|_|     |___/"]
-    msgtxt += ['']
+    msgtxt += [None]
     msgtxt += ['Version: {}'.format(VERSION)]
     msgtxt += ['Start of execution: {}'.format(timestart)]
     msgtxt += ['Python version: {}'.format(pyversion)]
     msgtxt += ['Running in directory: {}'.format(rundir)]
     msgtxt += ['Input file: {}'.format(infile)]
     msgtxt += ['Log file: {}'.format(logfile)]
-    msgtxt += ['']
+    msgtxt += [None]
     for txt in msgtxt:
         print_to_screen(txt)
-        logger.info(txt)
+        if txt is not None:
+            logger.info(txt)
 
 
 def bye_bye_world():
