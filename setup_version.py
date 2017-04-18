bdb285239bba7bde70c9c55f3205ac4a3faa6724
Anders Lervik <anders.lervik@ntnu.no>
2017-04-18 15:50:50 +0200

    Updated version.

--- setup_version.py
+++ setup_version.py
@@ -15,7 +15,7 @@ import subprocess
 MAJOR = 0
 MINOR = 9
 MICRO = 0
-DEV = 0
+DEV = 1
 ISRELEASED = False
 if not ISRELEASED:
     VERSION = '{:d}.{:d}.{:d}.dev{:d}'.format(MAJOR, MINOR, MICRO, DEV)
