039b29490851733cf5e76c2c39e0372385b950ac
Anders Lervik <andersle@gmail.com>
2017-09-26 18:22:01 +0200

    Fixed pep8 error in "aprogram.py"

--- test/engines/aprogram.py
+++ test/engines/aprogram.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     print('This is a program for testing external commands')
     args = sys.argv
     if len(args) > 1:
-        print('ERROR: Program were given arguments!', file=sys.stderr, end='\n')
+        print('ERROR: Program were given arguments!', file=sys.stderr,
+              end='\n')
         sys.exit(1)
     else:
         sys.exit(0)
